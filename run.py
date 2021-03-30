9879966bb9bb4a134d39343f0c63bda6beb349a6
Richard Wan <richard.wan@cabital.com>
2021-03-30 14:53:03 +0800

    1

--- run.py
+++ run.py
@@ -18,12 +18,8 @@ citizenCountryCodeList = get_json()['citizenCountryCodeList']
 
 if __name__ == '__main__':
     pytest.main(['TestCase/TestApiCase/TestCase.py', '-v', '--alluredir', './Reports'])
-    split = "/usr/share/allure generate ./Reports  -o ./Reports/html --clean"
+    split = "/usr/share/allure/allure generate ./Reports  -o ./Reports/html --clean"
     print(os.system('pwd'))
-    print(os.system('type allure'))
-    print(os.system('ls -l /usr/bin/allure'))
-    print(os.system('ls -l /usr/local/bin/allure'))
-    print(os.system('ls -l //usr/share/allure'))
     os.system(split)
 
 
