ea5d4aaac4a25f45b28ae80078d8834c96618cf2
Richard Wan <richard.wan@cabital.com>
2021-03-30 14:49:23 +0800

    no message

--- run.py
+++ run.py
@@ -20,6 +20,10 @@ if __name__ == '__main__':
     pytest.main(['TestCase/TestApiCase/TestCase.py', '-v', '--alluredir', './Reports'])
     split = "/usr/share/allure generate ./Reports  -o ./Reports/html --clean"
     print(os.system('pwd'))
+    print(os.system('type allure'))
+    print(os.system('ls -l /usr/bin/allure'))
+    print(os.system('ls -l /usr/local/bin/allure'))
+    print(os.system('ls -l //usr/share/allure'))
     os.system(split)
 
 
