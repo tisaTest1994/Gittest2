d1d13c07219d1657ce759d650a54190e1428550e
Richard Wan <richard.wan@cabital.com>
2021-04-27 17:14:35 +0800

    mysql test

--- Function/connect_mysql.py
+++ Function/connect_mysql.py
@@ -1,11 +1,16 @@
-import pymysql
+import pymysql.cursors
 from Function.common_function import *
 
 
-def connect_mysql():
+def connect_mysql(db, sql):
     account = get_json()['mysql']['account']
     password = get_json()['mysql']['password']
     host = get_json()['mysql']['host']
     port = get_json()['mysql']['port']
-    con = pymysql.connect(host=host, user=account, passwd=password, db='account', port=port, charset='utf8')
-    print(con)
+    connection = pymysql.connect(host=host, user=account, passwd=password, db=db, port=port, charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(sql, ('webmaster@python.org',))
+            result = cursor.fetchone()
+            print(result)
+            return result
--- run.py
+++ run.py
@@ -18,7 +18,7 @@ citizenCountryCodeList = get_json()['citizenCountryCodeList']
 
 
 if __name__ == '__main__':
-    connect_mysql()
+    connect_mysql('account', 'select * from account.account where account_id = "6cdfb1da-7646-4806-9a6a-445101928dc6";')
     # pytest.main(['TestCase/TestApiCase', '-n 8', '-v', '--alluredir', './Reports'])
     # split = "allure generate ./Reports  -o ./Reports/html --clean"
     # os.system(split)
