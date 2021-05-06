e08185f7a7be048295a7924436111f8edb3b5935
Richard Wan <richard.wan@cabital.com>
2021-05-06 16:09:27 +0800

    111

--- TestCase/TestApiCase/test_asset.py
+++ TestCase/TestApiCase/test_asset.py
@@ -6,21 +6,29 @@ import allure
 
 # asset相关cases
 def get_crypto_abs_amount(type, account=email['email'], password=email['password']):
-        accessToken = AccountFunction.get_account_token(account=account, password=password)['accessToken']
-        headers['Authorization'] = "Bearer " + accessToken
-        headers['X-Currency'] = 'USD'
-        r = session.request('GET', url='{}/core/account'.format(env_url), headers=headers)
-        for i in r.json()['wallets']:
-            if i['code'] == type:
-                return i['abs_amount']
+    accessToken = AccountFunction.get_account_token(account=account, password=password)['accessToken']
+    headers['Authorization'] = "Bearer " + accessToken
+    headers['X-Currency'] = 'USD'
+    r = session.request('GET', url='{}/core/account'.format(env_url), headers=headers)
+    for i in r.json()['wallets']:
+        if i['code'] == type:
+            return i['abs_amount']
+
+
+def get_crypto_quote(type, time):
+    connect_mysql()
 
 
 class TestAssetApi:
 
-    @allure.testcase('test_asset_001 查询单笔交易')
+    @allure.testcase('test_asset_001 查询每个币种当前资产市值')
     def test_asset_001(self):
-        with allure.step("获得token"):
-            accessToken = AccountFunction.get_account_token(account=email['email'], password=email['password'])[
-                'accessToken']
-        with allure.step("把token写入headers"):
-            headers['Authorization'] = "Bearer " + accessToken
+        crypto_list = get_json()['crypto_list']
+        for i in crypto_list:
+            print(get_crypto_abs_amount(i))
+
+    @allure.testcase('test_asset_002 查询每个币种今日损益')
+    def test_asset_002(self):
+        crypto_list = get_json()['crypto_list']
+        for i in crypto_list:
+            print(get_crypto_abs_amount(i))
