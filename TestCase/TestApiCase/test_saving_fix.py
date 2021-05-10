01cae46477cb196ba3f04ab51ec935980dd277b4
Richard Wan <richard.wan@cabital.com>
2021-05-10 11:06:29 +0800

    123

--- TestCase/TestApiCase/test_saving_fix.py
+++ TestCase/TestApiCase/test_saving_fix.py
@@ -26,5 +26,37 @@ class TestSavingFixApi:
         with allure.step("获取定期产品列表"):
             r = session.request('GET', url='{}/earn/fix/products'.format(env_url), headers=headers)
         for i in r.json():
-            print(i)
+            for y in i['products']:
+                logger.info('定期产品product_id是{}'.format(y['product_id']))
+                with allure.step("获取定期产品详情"):
+                    r = session.request('GET', url='{}/earn/fix/products/{}'.format(env_url, y['product_id']), headers=headers)
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert 'Earn Bit Fixed' in r.text, '获取定期产品详情失败,返回值是{}'.format(r.json())
 
+    @allure.testcase('test_saving_fix_003 获取定期产品利息列表')
+    def test_saving_fix_003(self):
+        with allure.step("获取定期产品列表"):
+            r = session.request('GET', url='{}/earn/fix/products'.format(env_url), headers=headers)
+        for i in r.json():
+            for y in i['products']:
+                logger.info('定期产品product_id是{}'.format(y['product_id']))
+                with allure.step("获取定期产品详情"):
+                    params = {
+                        'cursor': "0",
+                        'size': 99999
+                    }
+                    r = session.request('GET', url='{}/earn/products/{}/interests'.format(env_url, y['product_id']), params=params, headers=headers)
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        for z in r.json()['items']:
+                            assert z['apy'] is not None, "获取定期产品利息列表失败，返回值是{}".format(r.text)
+                            assert z['date'] is not None, "获取定期产品利息列表失败，返回值是{}".format(r.text)
