6f5cbdeb4d6a2c78cde392d91f5692e249139a38
Richard Wan <richard.wan@cabital.com>
2021-05-20 10:44:12 +0800

    1314

--- TestCase/TestApiCase/test_convert_order.py
+++ TestCase/TestApiCase/test_convert_order.py
@@ -16,22 +16,23 @@ class TestConvertOrderApi:
             if info is not None:
                  cfx_info.append(info)
         print(cfx_info)
+        cfx_info = []
         cfx_list = []
         # for y in cfx_info:
         #     cfx_dict = {}
         #     if y['book_id'] == 1:
         #         profit = Decimal(y['trading_amount']) * (Decimal(y['rate']) - Decimal(y['cost']))
         #         profit = '{}.{}'.format(str(profit).split('.')[0], str(profit).split('.')[1][:6])
-        #         assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
+        #         #assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
         #     elif y['book_id'] == 2:
         #         profit = Decimal(y['trading_amount']) * (Decimal(y['rate']) - Decimal(y['cost']))
         #         profit = '{}.{}'.format(str(profit).split('.')[0], str(profit).split('.')[1][:8])
-        #         assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
+        #         #assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
         #         print(profit)
         #         print(y)
         #     elif y['book_id'] == 3:
         #         profit = Decimal(y['trading_amount']) * (Decimal(y['rate']) - Decimal(y['cost']))
         #         profit = '{}.{}'.format(str(profit).split('.')[0], str(profit).split('.')[1][:6])
-        #         assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
+        #         #assert profit == y['gnl'], '预计损益是{}，数据库返回是{}'.format(profit, y['gnl'])
         #         print(profit)
         #         print(y)
