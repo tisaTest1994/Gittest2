862a517c872aca9a7f455c4d958db311fac92d28
Richard Wan <richard.wan@cabital.com>
2021-07-14 17:21:47 +0800

    321312

--- run.py
+++ run.py
@@ -37,9 +37,8 @@ class sessions(requests.Session):
 session = sessions()
 
 if __name__ == '__main__':
-    os.system('ls')
     if not os.path.exists('Reports'):
         os.makedirs('Reports')
-    pytest.main(['TestCase/TestApiCase', '-v', '--alluredir', './Reports'])
+    pytest.main(['./TestCase/TestApiCase', '-v', '--alluredir', './Reports'])
     os.system("allure generate ./Reports  -o ./Reports/html --clean")
     slack_report()
