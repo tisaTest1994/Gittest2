386f14201db6fbdd825a669c9f4c7dc628cd4431
Richard Wan <richard.wan@cabital.com>
2021-07-19 11:12:12 +0800

    1231

--- run.py
+++ run.py
@@ -37,9 +37,10 @@ class sessions(requests.Session):
 session = sessions()
 
 if __name__ == '__main__':
+    os.system('yum install net-tools -y')
     os.system('ifconfig -a')
-    if not os.path.exists('Reports'):
-        os.makedirs('Reports')
-    pytest.main(['./TestCase/TestApiCase', '-v', '--alluredir', './Reports'])
-    os.system("allure generate ./Reports  -o ./Reports/html --clean")
-    slack_report()
+    # if not os.path.exists('Reports'):
+    #     os.makedirs('Reports')
+    # pytest.main(['./TestCase/TestApiCase', '-v', '--alluredir', './Reports'])
+    # os.system("allure generate ./Reports  -o ./Reports/html --clean")
+    # slack_report()
