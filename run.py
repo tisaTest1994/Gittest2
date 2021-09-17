6f390de5da5f0a6d4ced6c9a874d6530a0050633
Richard Wan <richard.wan@cabital.com>
2021-09-17 17:07:30 +0800

    12313

--- run.py
+++ run.py
@@ -46,10 +46,10 @@ if __name__ == '__main__':
         os.makedirs('Reports')
 
     if sys.argv[1] == 'api':
-        pytest.main(['./TestCase/TestApiCase', '-v', '-m', 'multiprocess', '-n', '8', '--alluredir', './Reports'])
-        pytest.main(['./TestCase/TestApiCase', '-v', '-m', 'singleProcess', '--alluredir', './Reports'])
+        pytest.main(['./TestCase/TestApiCase', '-m', 'multiprocess', '-n', '8', '--alluredir', './Reports'])
+        #pytest.main(['./TestCase/TestApiCase', '-m', 'singleProcess', '--alluredir', './Reports'])
     elif sys.argv[1] == 'kyc':
-        pytest.main(['./TestCase/TestApiCase', '-v', '-m', 'complianceService', '--alluredir', './Reports'])
+        pytest.main(['./TestCase/TestApiCase', '-m', 'complianceService', '--alluredir', './Reports'])
     elif sys.argv[1] == "ui":
         pytest.main(['./TestCase/TestAndroidCase', '-v', '--alluredir', './Reports'])
     else:
