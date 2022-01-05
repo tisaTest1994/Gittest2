b9ca232131e5f93470d9785f2fcd57987cad5773
Richard Wan <richard.wan@cabital.com>
2022-01-05 18:55:50 +0800

    test all

--- TestCase/TestConnectCase/test_connectTransaction.py
+++ TestCase/TestConnectCase/test_connectTransaction.py
@@ -688,3 +688,56 @@ class TestConnectTransactionApi:
             with allure.step("校验返回值"):
                 assert r.json()['status'] == 'SUCCESS', "从cabital转移到bybit账户并且关联C+T交易，Direct Debit的金额 必须小于等于 cfx交易的金额错误，返回值是{}".format(r.text)
                 sleep(30)
+
+    @allure.testcase('test_connect_transaction_015 把数字货币从bybit转移到cabital账户')
+    def test_connect_transaction_015(self):
+        with allure.step("测试用户的account_id"):
+            account_id = get_json()['email']['accountId']
+        with allure.step("从配置接口获取可以划转的数据"):
+            with allure.step("验签"):
+                unix_time = int(time.time())
+                nonce = generate_string(30)
+                sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='GET', url='/api/v1/config',
+                                                    nonce=nonce)
+                connect_headers['ACCESS-SIGN'] = sign
+                connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+                connect_headers['ACCESS-NONCE'] = nonce
+            with allure.step("获取合作方的配置"):
+                r = session.request('GET', url='{}/config'.format(self.url), headers=connect_headers)
+                for i in r.json()['currencies']:
+                    if i['config']['credit']['allow']:
+                        with allure.step("获得转移前cabital内币种balance金额"):
+                            balance_old = ApiFunction.get_crypto_number(type=i['symbol'])
+                        with allure.step("获得data"):
+                            data = {
+                                'amount': str(Decimal(i['config']['credit']['max']) + Decimal('2.456')),
+                                'symbol': i['symbol'],
+                                'direction': 'CREDIT',
+                                'external_id': generate_string(15)
+                            }
+                        with allure.step("验签"):
+                            unix_time = int(time.time())
+                            nonce = generate_string(30)
+                            sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='POST',
+                                                                url='/api/v1/accounts/{}/transfers'.format(account_id),
+                                                                nonce=nonce,
+                                                                body=json.dumps(data))
+                            connect_headers['ACCESS-SIGN'] = sign
+                            connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+                            connect_headers['ACCESS-NONCE'] = nonce
+                        with allure.step("把数字货币从cabital转移到bybit账户"):
+                            r = session.request('POST',
+                                                url='{}/accounts/{}/transfers'.format(self.url, account_id),
+                                                data=json.dumps(data), headers=connect_headers)
+                        with allure.step("状态码和返回值"):
+                            logger.info('状态码是{}'.format(str(r.status_code)))
+                            logger.info('返回值是{}'.format(str(r.text)))
+                        with allure.step("校验状态码"):
+                            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                        with allure.step("校验返回值"):
+                            assert r.json()['status'] == 'SUCCESS', "把{}从cabital转移到bybit账户错误，返回值是{}".format(i['symbol'], r.text)
+                        with allure.step("获得转移后cabital内币种balance金额"):
+                            sleep(30)
+                            balance_latest = ApiFunction.get_crypto_number(type=i['symbol'])
+                        assert Decimal(balance_old) + Decimal(data['amount']) == Decimal(
+                            balance_latest), "把{}从bybit转移到cabital账户错误，转移前balance是{},转移后balance是{}".format(i['symbol'], balance_old, balance_latest)
