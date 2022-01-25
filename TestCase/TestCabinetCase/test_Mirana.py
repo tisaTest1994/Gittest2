9d7387ce2971444958b01d39252412b6b87de2ae
Winnie <winnie.wang@cabital.com>
2022-01-25 10:26:26 +0800

    Mirana/Rebalance update

--- TestCase/TestCabinetCase/test_Mirana.py
+++ TestCase/TestCabinetCase/test_Mirana.py
@@ -11,7 +11,8 @@ class TestOperateApi:
             account=get_json()['operate_admin_account']['email'],
             password=get_json()['operate_admin_account']['password'], type='operate')
 
-    @allure.title('test_2B_001 提现USDC成功')
+    @allure.title('test_2B_001')
+    @allure.description('提现USDC成功')
     def test_2B_001(self):
         with allure.step("提现USDC成功"):
             data = {
@@ -32,7 +33,8 @@ class TestOperateApi:
         with allure.step("校验返回值"):
             assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
 
-    @allure.title('test_2B_002 申购活期产品成功')
+    @allure.title('test_2B_002')
+    @allure.description('申购活期产品成功')
     def test_2B_002(self):
         with allure.step("申购活期产品成功"):
             data = {
@@ -53,7 +55,8 @@ class TestOperateApi:
         with allure.step("校验返回值"):
             assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
 
-    @allure.title('test_2B_003 申购定期产品成功')
+    @allure.title('test_2B_003')
+    @allure.description('申购定期产品成功')
     def test_2B_003(self):
         with allure.step("申购定期产品成功"):
             data = {
@@ -74,7 +77,8 @@ class TestOperateApi:
         with allure.step("校验返回值"):
             assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
 
-    @allure.title('test_2B_004 赎回活期产品成功')
+    @allure.title('test_2B_004')
+    @allure.description('赎回活期产品成功')
     def test_2B_004(self):
         with allure.step("赎回活期产品成功"):
             data = {
@@ -95,17 +99,36 @@ class TestOperateApi:
         with allure.step("校验返回值"):
             assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
 
-    @allure.title('test_2B_005 定期产品打开auto renew')
+    @allure.title('test_2B_005')
+    @allure.description('申购定期产品后打开auto renew')
     def test_2B_005(self):
+        with allure.step("申购定期产品成功"):
+            data = {
+                "amount": "1"
+            }
+            r = session.request('POST', url='{}/operatorapi/account/{}/subscribe/{}'.format(operateUrl,
+                                                                                            get_json()['2B'][
+                                                                                                'mirana_account'],
+                                                                                            get_json()['2B'][
+                                                                                                'fixed_product']),
+                                data=json.dumps(data),
+                                headers=headers)
+        with allure.step("状态码和返回值"):
+            logger.info('状态码是{}'.format(str(r.status_code)))
+            logger.info('返回值是{}'.format(str(r.text)))
+        with allure.step("校验状态码"):
+            assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+        with allure.step("校验返回值"):
+            assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
         with allure.step("定期产品打开auto renew"):
+            transaction_id = r.json()['transaction_id']
             data = {
                 "auto_renew": True
             }
             r = session.request('POST', url='{}/operatorapi/account/{}/renew/{}'.format(operateUrl,
                                                                                         get_json()['2B'][
                                                                                             'mirana_account'],
-                                                                                        get_json()['2B'][
-                                                                                            'txn_id']),
+                                                                                        transaction_id),
                                 data=json.dumps(data),
                                 headers=headers)
         with allure.step("状态码和返回值"):
@@ -114,19 +137,38 @@ class TestOperateApi:
         with allure.step("校验状态码"):
             assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
-            assert 'update_result' in r.text, "查询update_result错误，返回值是{}".format(r.text)
+            assert r.json()['update_result'] == True, "查询update_result错误，返回值是{}".format(r.text)
 
-    @allure.title('test_2B_006 定期产品关闭auto renew')
+    @allure.title('test_2B_006')
+    @allure.description('申购定期后产品关闭auto renew')
     def test_2B_006(self):
-        with allure.step("定期产品关闭auto renew"):
+        with allure.step("申购定期产品成功"):
+            data = {
+                "amount": "1.12"
+            }
+            r = session.request('POST', url='{}/operatorapi/account/{}/subscribe/{}'.format(operateUrl,
+                                                                                            get_json()['2B'][
+                                                                                                'mirana_account'],
+                                                                                            get_json()['2B'][
+                                                                                                'fixed_product']),
+                                data=json.dumps(data),
+                                headers=headers)
+        with allure.step("状态码和返回值"):
+            logger.info('状态码是{}'.format(str(r.status_code)))
+            logger.info('返回值是{}'.format(str(r.text)))
+        with allure.step("校验状态码"):
+            assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+        with allure.step("校验返回值"):
+            assert 'transaction_id' in r.text, "查询transaction_id错误，返回值是{}".format(r.text)
+        with allure.step("定期产品打开auto renew"):
+            transaction_id = r.json()['transaction_id']
             data = {
                 "auto_renew": False
             }
             r = session.request('POST', url='{}/operatorapi/account/{}/renew/{}'.format(operateUrl,
                                                                                         get_json()['2B'][
                                                                                             'mirana_account'],
-                                                                                        get_json()['2B'][
-                                                                                            'txn_id']),
+                                                                                        transaction_id),
                                 data=json.dumps(data),
                                 headers=headers)
         with allure.step("状态码和返回值"):
@@ -135,4 +177,4 @@ class TestOperateApi:
         with allure.step("校验状态码"):
             assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
-            assert 'update_result' in r.text, "查询update_result错误，返回值是{}".format(r.text)
+            assert r.json()['update_result'] == True, "查询update_result错误，返回值是{}".format(r.text)
