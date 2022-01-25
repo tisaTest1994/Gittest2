696b93658283fa675b0ff5194b3ce8ef0bbae7a2
Yanting <yanting.huang@cabital.com>
2022-01-25 14:27:19 +0800

    web

--- Function/web_common_function.py
+++ Function/web_common_function.py
@@ -23,6 +23,14 @@ def check_web(driver, page, text):
             if driver.find_element_by_name(text).is_displayed():
                 logger.info('在{}页面上寻找到了{}'.format(page, text))
                 return True
+        elif type == 'class':
+            if driver.find_element_by_class_name(text).is_displayed():
+                logger.info('在{}页面上寻找到了{}'.format(page, text))
+                return True
+        else:
+            if driver.find_element_by_xpath(("//*[@" + type + '="{}"]').format(text)).is_displayed():
+                logger.info('在{}页面上寻找到了{}'.format(page, text))
+                return True
         wait_time = wait_time + 1
     return False
 
@@ -41,6 +49,9 @@ def operate_element(driver, page, text, type='click', input='', cycle=15):
             elif type == 'delete':
                 for i in range(cycle):
                     driver.find_element_by_id(text).send_keys(Keys.BACKSPACE)
+            elif type == "clear2":
+                js = 'document.querySelector("#' + text + '").value="";'
+                driver.execute_script(js)
         elif element_type == 'alt':
             if type == 'click':
                 driver.find_element_by_xpath('//*[@alt="{}"]'.format(text)).click()
@@ -61,6 +72,10 @@ def operate_element(driver, page, text, type='click', input='', cycle=15):
             elif type == 'delete':
                 for i in range(cycle):
                     driver.find_element_by_name(text).send_keys(Keys.BACKSPACE)
+            elif type == 'is_display':
+                driver.find_element_by_name(text)
+            elif type == 'check':
+                driver.find_element_by_name(text)
 
 
 # 获取页面元素的text
@@ -68,11 +83,13 @@ def get_element_text(driver, page, text):
     element_type = get_json(file='web_tree.json')[page][text]
     if element_type == 'id':
         return driver.find_element_by_id(text).text
+    elif element_type == 'class':
+        return driver.find_element_by_class_name(text).text
+    else:
+        return driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, text)).text
 
 
 # 图像识别
 def check_web_photo(driver, photo_name):
     driver.assert_template(Template("{}/{}".format(get_photo(), photo_name)), "验证图片{}是否存在".format(photo_name))
-
-
 
