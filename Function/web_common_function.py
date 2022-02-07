4f66680f88a039aaa70af74980832c5993578730
Yanting <yanting.huang@cabital.com>
2022-02-07 14:30:15 +0800

    test

--- Function/web_common_function.py
+++ Function/web_common_function.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 
 # 操作页面元素
-def operate_element_web(driver, page, element_string, type='click', input_string='', cycle=15):
+def operate_element_web(driver, page, element_string, type='click', input_string=''):
     element_type = get_json(file='web_tree.json')[page][element_string]
     if type == 'click':
         if element_type == 'id':
@@ -14,7 +14,7 @@ def operate_element_web(driver, page, element_string, type='click', input_string
         elif element_type == 'name':
             driver.find_element_by_name(element_string).click()
         else:
-            driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).click()
+            driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).click()
         logger.info('点击{}页面{}元素'.format(page, element_string))
     elif type == 'input':
         if element_type == 'id':
@@ -22,7 +22,7 @@ def operate_element_web(driver, page, element_string, type='click', input_string
         elif element_type == 'name':
             driver.find_element_by_name(element_string).send_keys(input_string)
         else:
-            driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).send_keys(input_string)
+            driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).send_keys(input_string)
         logger.info("点击{}页面{}元素,输入{}".format(page, element_string, input_string))
     elif type == 'check':
         if element_type == 'id':
@@ -34,24 +34,26 @@ def operate_element_web(driver, page, element_string, type='click', input_string
             logger.info('{}页面{}元素是否存在{}'.format(page, element_string, result))
             return result
         else:
-            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).is_displayed()
+            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).is_displayed()
             logger.info('{}页面{}元素是否存在{}'.format(page, element_string, result))
             return result
     elif type == 'delete':
-        for i in range(cycle):
-            if element_type == 'id':
-                driver.find_element_by_id(element_string).send_keys(Keys.BACKSPACE)
-            elif element_type == 'name':
-                driver.find_element_by_name(element_string).send_keys(Keys.BACKSPACE)
-            else:
-                driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).send_keys(Keys.BACKSPACE)
+        if element_type == 'id':
+            driver.find_element_by_id(element_string).send_keys(Keys.COMMAND+'a')
+            driver.find_element_by_id(element_string).send_keys(Keys.BACKSPACE)
+        elif element_type == 'name':
+            driver.find_element_by_id(element_string).send_keys(Keys.COMMAND+'a')
+            driver.find_element_by_name(element_string).send_keys(Keys.BACKSPACE)
+        else:
+            driver.find_element_by_id(element_string).send_keys(Keys.COMMAND+'a')
+            driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).send_keys(Keys.BACKSPACE)
     elif type == 'get_text':
         if element_type == 'id':
             return driver.find_element_by_id(element_string).text
         elif element_type == 'name':
             return driver.find_element_by_name(element_string).text
         else:
-            return driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).text
+            return driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).text
     elif type == 'check_enabled':
         if element_type == 'id':
             result = driver.find_element_by_id(element_string).is_enabled()
@@ -62,7 +64,7 @@ def operate_element_web(driver, page, element_string, type='click', input_string
             logger.info('{}页面{}元素是否可被修改{}'.format(page, element_string, result))
             return result
         else:
-            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).is_enabled()
+            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).is_enabled()
             logger.info('{}页面{}元素是否可被修改{}'.format(page, element_string, result))
             return result
     elif type == 'check_selected':
@@ -75,13 +77,14 @@ def operate_element_web(driver, page, element_string, type='click', input_string
             logger.info('{}页面{}元素是否被选中{}'.format(page, element_string, result))
             return result
         else:
-            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string)).is_selected()
+            result = driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string)).is_selected()
             logger.info('{}页面{}元素是否被选中{}'.format(page, element_string, result))
             return result
     else:
-        return driver.find_element_by_xpath('//*[@{}="{}"]'.format(type, element_string))
+        return driver.find_element_by_xpath('//*[@{}="{}"]'.format(element_type, element_string))
 
 
 # 图像识别
 def check_web_photo(driver, photo_name):
     driver.assert_template(Template("{}/{}".format(get_photo(), photo_name)), "验证图片{}是否存在".format(photo_name))
+
--- Function/web_function.py
+++ Function/web_function.py
@@ -35,11 +35,11 @@ class webFunction:
         with allure.step("输入密码"):
             operate_element_web(driver, 'loginPage', 'sign_password_input', type='delete')
             operate_element_web(driver, 'loginPage', 'sign_password_input', type='input', input_string=password)
-        with allure.step("点击sign in"):
+        with allure.step("点击log in"):
             operate_element_web(driver, 'loginPage', 'login_login')
             sleep(2)
         with allure.step("确定已经登录成功到首页"):
-            assert operate_element_web(driver, 'assetPage', 'Cabital Logo'), '已经成功登录'
+            assert operate_element_web(driver, 'assetPage', 'Cabital Logo', type='check'), '未成功登录'
             sleep(2)
 
     # 登出 web
@@ -47,6 +47,36 @@ class webFunction:
     def logout_web(driver):
         with allure.step("点击登出"):
             operate_element_web(driver, 'assetPage', 'header-btn-hi-nickname')
-            operate_element_web(driver, 'assetPage', 'header-btn-user-signout')
+            assert operate_element_web(driver, 'assetPage', 'dialog_forgetpassword')
+
+    # 注册 web
+    @staticmethod
+    def signup_web(self):
+        test_account = generate_email()
+        email_code = 666666
+        pword = get_json()['web'][get_json()['env']]['password']
+        with allure.step("确定打开登录页面"):
+            assert operate_element_web(self.driver, 'loginPage', 'signinForm', type='check'), '未打开登录页面'
+        with allure.step("填写注册信息"):
+            # 点击signup按钮，跳转至signup页面
+            operate_element_web(self.driver, 'loginPage', "login_gotosignup")
+            # 输入账号
+            operate_element_web(self.driver, 'signupPage', "signup_form_password_email", "input", test_account)
+            # 点击发送验证码
+            operate_element_web(self.driver, 'signupPage', "signup_sendcode")
+            # 输入验证码
+            operate_element_web(self.driver, 'signupPage', "mailcode", "input", email_code)
+            # 输入密码
+            operate_element_web(self.driver, 'signupPage', "signup_form_password_input", "input", pword)
+            # 勾选复选框
+            operate_element_web(self.driver, 'signupPage', "signup_termschk")
+            # 点击注册按钮
+            operate_element_web(self.driver, 'signupPage', "signup_registernow")
+            time.sleep(2)
+        with allure.step("确定注册成功"):
+            account_name = test_account.split('@')
+            operate_element_web(self.driver, "AccountSetPage", "mainpage_menu_account")
+            assert operate_element_web(self.driver, "AccountSetPage", "account-nickname", "get_text") ==\
+                   account_name[0], "注册失败"
 
 
