410fc7c3654cfce149044155e908ab37720a499c
alice <alice.wang@cabital.com>
2022-02-08 14:54:51 +0800

    update app

--- TestCase/TestAndroidCase/test_account_ui.py
+++ TestCase/TestAndroidCase/test_account_ui.py
@@ -1,6 +1,7 @@
 from Function.ui_function import *
 import allure
 from Function.api_common_function import *
+from TestCase.TestApiCase.test_asset import *
 
 
 class TestAccountUi:
@@ -8,6 +9,8 @@ class TestAccountUi:
     def setup_method(self):
         with allure.step("打开 app"):
             start_app(package_name)
+        with allure.step("登录客户账户获得后续操作需要的token"):
+            ApiFunction.add_headers()
 
     def teardown(self):
         with allure.step("关闭 app"):
@@ -52,16 +55,46 @@ class TestAccountUi:
                 with allure.step("输入邮箱验证码"):
                     text(get_json()['web'][get_json()['env']]['code'])
                 # assert断言Next按钮可点击
+                with allure.step("判断是否可点击Next"):
+                    assert operate_element_app('signupPage', "Next", type='check_enabled'), '没有到达{}页面或者找不到{}页面元素'.format('signupPage', 'Next')
                 with allure.step("点击Next"):
                     operate_element_app('signupPage', "Next", type='click')
                 with allure.step("断言进入设置密码页面"):
                     assert operate_element_app('signupPage', 'Set login password', type='check'), '没有到达{}页面或者找不到{}页面元素'.format('signupPage', 'Set login password')
                 with allure.step("设置密码"):
                     operate_element_app('signupPage', 'password', type='input', input_string=get_json()['web'][get_json()['env']]['password'])
+                # with allure.step("断言密码设置成功，高亮显示3个规则"):
+                #     assert operate_element_app('signupPage', 'password rule box', type='check_selected'), '没有到达{}页面或者找不到{}页面元素'.format('signupPage', 'password rule box')
                 with allure.step("点击Confirm Password"):
                     operate_element_app('signupPage', "Confirm Password", type='click')
                 with allure.step("断言进入设置密码页面"):
                     assert operate_element_app('signupPage', 'Total Assert Value', type='check'), '没有到达{}页面或者找不到{}页面元素'.format('signupPage', 'Total Assert Value')
+            with allure.step("登出"):
+                UiFunction.logout()
+                sleep(3)
+    @allure.title('test_account_003')
+    @allure.description('portfolio total asset value')
+    def test_account_003(self):
+        with allure.step("登录"):
+            UiFunction.login(account=get_json()['email']['email'], password=get_json()['email']['password'])
+            sleep(5)
+        with allure.step("进入portfolio页面点击view"):
+            operate_element_app('portfolioPage', "View", type='click')
+        with allure.step("断言进入Asset overview页面"):
+            assert operate_element_app('portfolioPage', 'Asset Allocation', type='check'), '没有到达{}页面或者找不到{}页面元素'.format( 'portfolioPage', 'Asset Allocation')
+        with allure.step("获取Total Asset value"):
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
