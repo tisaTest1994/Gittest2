4ec2a030d13ff4cf56b08813c124efd5b26b520b
Richard Wan <richard.wan@cabital.com>
2022-02-08 17:53:35 +0800

    test 1

--- Function/web_function.py
+++ Function/web_function.py
@@ -10,11 +10,13 @@ class webFunction:
         # os.path.abspath()获得绝对路径
         path = os.path.abspath(os.path.dirname(__file__))
         options = webdriver.ChromeOptions()
-        options.add_argument('--disable-gpu')
+        #options.add_argument('--disable-gpu')
         # 指定浏览器的分辨率
         options.add_argument('--window-size=1920,1080')
         # 无界面运行
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
+        options.add_argument("--no-sandbox")
+        options.add_argument('disable-infobars')
         # 判断运行环境
         if 'mac' in str(platform.platform()):
             driver = WebChrome(executable_path=path + "/../Resource/chromedriver_mac", chrome_options=options)
