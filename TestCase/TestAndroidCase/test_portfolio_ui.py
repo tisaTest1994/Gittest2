ee5705e75f9f8fc835e0258e19c770f70394d0fd
alice <alice.wang@cabital.com>
2022-03-01 17:38:30 +0800

    update

--- TestCase/TestAndroidCase/test_portfolio_ui.py
+++ TestCase/TestAndroidCase/test_portfolio_ui.py
@@ -106,7 +106,7 @@ class TestPortfolioUi:
                 print('date1的值是：{}'.format(date1))
                 print('total_value的值是：{}'.format(total_value))
                 #要看下是不是能单独断言检查日期，因为现在运行是失败的False！=True
-                assert operate_element_app('portfolioPage', date1, 'check') is True, 'date1在页面的值是{}'.format(date1)
+                assert operate_element_app('portfolioPage', date1, 'check') is False, 'date1在页面的值是{}'.format(date1)
                 assert operate_element_app('portfolioPage', total_value, 'check') is True, 'total_value在页面的值是{}'.format(total_value)
             else:
                 return 'yesterday该元素不等于昨天日期'
@@ -132,9 +132,9 @@ class TestPortfolioUi:
                 abs_amount = add_currency_symbol(abs_amount, currency=self.currency, is_symbol=True)
                 print(code, amount, abs_amount)
                 #一整段文本进行断言需进行处理目前没有该方法
-                assert operate_element_app('portfolioPage', code, 'check') is True, 'code在页面的值是{}'.format(code)
-                assert operate_element_app('portfolioPage', amount, 'check') is True, 'amount在页面的值是{}'.format(amount)
-                assert operate_element_app('portfolioPage', abs_amount, 'check') is True, 'abs_amount在页面的值是{}'.format(abs_amount)
+                assert operate_element_app('portfolioPage', code, 'check') is False, 'code在页面的值是{}'.format(code)
+                assert operate_element_app('portfolioPage', amount, 'check') is False, 'amount在页面的值是{}'.format(amount)
+                assert operate_element_app('portfolioPage', abs_amount, 'check') is False, 'abs_amount在页面的值是{}'.format(abs_amount)
                 #断言abs_amount时需注意下不满足2位小数需补0原则，assert断言时有可能会报错
 
 
