9a91c7113d4f230ed79c8609a886b6fa4f133e42
Richard Wan <richard.wan@cabital.com>
2022-03-16 17:31:00 +0800

    12

--- TestCase/TestApiCase/test_core.py
+++ TestCase/TestApiCase/test_core.py
@@ -269,8 +269,8 @@ class TestCoreApi:
                                 for k in r4.json()['transactions']:
                                     fled_all_interest_amounts_list.append(Decimal(k['maturity_interest']['amount']))
                             quote = sqlFunction.get_now_quote('{}-{}'.format(x, i))
-                            fled_all_interest_list.append(add_currency_symbol(Decimal(quote['middle']) * sum(fled_all_interest_amounts_list), i))
-                        print(fled_all_interest_list)
+                            fled_all_interest_list.append(crypto_len(Decimal(quote['middle']) * sum(fled_all_interest_amounts_list), i))
+                    assert r.json()['cumulative_inerest'] == Decimal(sum(flexible_all_interest_list) + sum(fled_all_interest_list)), '获取所有Saving产品的持有金额详情错误，显示货币类型是{}，返回值是{}".format(i, r.text)'
 
 
 
