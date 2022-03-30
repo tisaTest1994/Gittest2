cc3326e004df1dccbf346748fbebf2aba39e550d
Richard Wan <richard.wan@cabital.com>
2022-03-30 17:32:04 +0800

    accounting

--- TestCase/TestAccountingCase/test_wallet.py
+++ TestCase/TestAccountingCase/test_wallet.py
@@ -1,3 +1,5 @@
+import json
+
 from Function.api_function import *
 from Function.operate_excel import *
 from Function.operate_sql import *
@@ -10,13 +12,13 @@ class TestAccountingApi:
     def setup_method(self):
         pass
 
-    def test_accounting_001(self):
-        for i in OperateExcel.get_excel_sheet_names():
-            print(i)
+    # def test_accounting_001(self):
+    #     for i in OperateExcel.get_excel_sheet_names():
+    #         print(i)
 
     def test_accounting_002(self):
         for i in range(1, OperateExcel.get_excel_sheet_all_row_number('Cash Wallet')):
             line_info = OperateExcel.get_excel_sheet_row('Cash Wallet', i)
-            sql = "select * from wallet where wallet_name = '{}';".format(line_info[3])
+            sql = "select * from wallet where wallet_name = {};".format(str(line_info[3]).split(':')[1])
             info = sqlFunction().connect_mysql('wallet', sql=sql)
             print(info)
