71b581c5af1cb35d69e7184d44ca85e4595442cd
Richard Wan <richard.wan@cabital.com>
2022-04-01 18:45:16 +0800

    123

--- TestCase/TestAccountingCase/test_wallet.py
+++ TestCase/TestAccountingCase/test_wallet.py
@@ -19,7 +19,27 @@ class TestAccountingApi:
     def test_accounting_002(self):
         for i in range(1, OperateExcel.get_excel_sheet_all_row_number('Cash Wallet')):
             line_info = OperateExcel.get_excel_sheet_row('Cash Wallet', i)
-            sql = "select * from wallet where wallet_name = {} and code = {};".format(str(line_info[3]).split(':')[1], str(line_info[5]).split(':')[1])
+            with allure.step("账户状态"):
+                if str(line_info[16]).split(':')[1] == '正常':
+                    status = 1
+                else:
+                    status = 2
+            with allure.step("账户状态"):
+                if str(line_info[16]).split(':')[1] == '正常':
+                    account_type = 1
+                else:
+                    account_type = 2
+            with allure.step("是否可透支"):
+                if 'YES' in str(line_info[15]).split(':')[1]:
+                    allow_overdraft = 1
+                else:
+                    allow_overdraft = 2
+            with allure.step("借贷关系"):
+                if 'Debit' in str(line_info[13]).split(':')[1]:
+                    balance_direction = 2
+                else:
+                    balance_direction = 1
+            sql = "select * from wallet where wallet_name = {} and code = {} and account_code = {} and status = {} and account_type = {} and allow_overdraft = {} and balance_direction = {};".format(str(line_info[3]).split(':')[1], str(line_info[5]).split(':')[1], str(line_info[10]).split(':')[1], status, account_type, allow_overdraft, balance_direction)
             info = sqlFunction().connect_mysql('wallet', sql=sql)
             print(info[0])
             print(info[0]['code'])
