b6f3bf1c88eeb36a55bcd3c2c3ec403630e9ff21
Winnie <winnie.wang@cabital.com>
2022-04-06 10:17:19 +0800

    用户补充信息

--- TestCase/TestApiCase/test_account.py
+++ TestCase/TestApiCase/test_account.py
@@ -879,7 +879,7 @@ class TestAccountApi:
         with allure.step("校验状态码"):
             assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
-            assert '"missing":[]' in r.text, "获取用户必填的KYC数据，获取全部信息失败，返回值是{}".format(r.text)
+            assert '"missing":null' in r.text, "获取用户必填的KYC数据，获取全部信息失败，返回值是{}".format(r.text)
 
     @allure.title('test_account_047')
     @allure.description('补充用户必填的kyc数据')
@@ -945,3 +945,16 @@ class TestAccountApi:
             assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
             assert r.json()['promotion_details'] is not None, "promo_code推广code失败，返回值是{}".format(r.text)
+
+    @allure.title('test_account_051')
+    @allure.description('获取用户全部补充信息')
+    def test_account_051(self):
+        with allure.step("获取用户必填的KYC数据，获取全部信息"):
+            r = session.request('GET', url='{}/account/additional/info'.format(env_url), headers=headers)
+        with allure.step("状态码和返回值"):
+            logger.info('状态码是{}'.format(str(r.status_code)))
+            logger.info('返回值是{}'.format(str(r.text)))
+        with allure.step("校验状态码"):
+            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+        with allure.step("校验返回值"):
+            assert r.json()['userId'] is not None and r.json()['additionalInfos'] is not None, "获取用户全部补充信息失败，返回值是{}".format(r.text)
