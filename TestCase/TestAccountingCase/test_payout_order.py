c3a0c68692d253c88603588d7db4e90afb99d5cb
Richard Wan <richard.wan@cabital.com>
2022-04-25 17:08:52 +0800

    123

--- TestCase/TestAccountingCase/test_payout_order.py
+++ TestCase/TestAccountingCase/test_payout_order.py
@@ -14,10 +14,14 @@ class TestAccountingPayOutOrderApi:
     @allure.description('wallet 验证')
     def test_payout_order_001(self):
         with allure.step("生成一笔ETH payout订单"):
-            transaction_id = ApiFunction.get_payout_transaction_id(amount='0.01', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='ETH')
-            print(transaction_id)
+            # transaction_id = ApiFunction.get_payout_transaction_id(amount='0.01', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='ETH')
+            transaction_id = 'f01f782f-b6ce-42be-b4bb-aa1f3504bf47'
         with allure.step("通过sql查询movement id"):
             sql = "select movement_id from movement where transaction_id = {}".format(transaction_id)
+            info = sqlFunction().connect_mysql('wallet', sql=sql)
+            print(info)
+            print(type(info))
+
 
 
 
