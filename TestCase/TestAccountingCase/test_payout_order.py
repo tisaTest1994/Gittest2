c00f78985897ad08dacd6ec4fd55adea8e6aab95
Richard Wan <richard.wan@cabital.com>
2022-04-27 14:09:46 +0800

    12345

--- TestCase/TestAccountingCase/test_payout_order.py
+++ TestCase/TestAccountingCase/test_payout_order.py
@@ -15,12 +15,12 @@ class TestAccountingPayOutOrderApi:
     def test_payout_order_001(self):
         with allure.step("生成一笔ETH payout订单"):
             # transaction_id = ApiFunction.get_payout_transaction_id(amount='0.01', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='ETH')
-            sleep(10)
+            # sleep(10)
             transaction_id = 'f01f782f-b6ce-42be-b4bb-aa1f3504bf47'
         with allure.step("确认 PAYOUT_TXN_STATUS_CREATED 冻账"):
             sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_CREATED';".format(transaction_id)
             movement_id_create = sqlFunction().connect_mysql('wallet', sql=sql)
-            assert movement_id_create == (), 'PAYOUT_TXN_STATUS_CREATED冻账失败'
+            assert movement_id_create != (), 'PAYOUT_TXN_STATUS_CREATED冻账失败'
         with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 内部户"):
             sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_EXECUTING' and offset = 1;".format(transaction_id)
             movement_id = sqlFunction().connect_mysql('wallet', sql=sql)
