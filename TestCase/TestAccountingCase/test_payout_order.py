bbcd0b91e971924d5f58d184f428fd64f6ca35fb
Richard Wan <richard.wan@cabital.com>
2022-04-27 17:23:22 +0800

    21312331

--- TestCase/TestAccountingCase/test_payout_order.py
+++ TestCase/TestAccountingCase/test_payout_order.py
@@ -11,7 +11,7 @@ class TestAccountingPayOutOrderApi:
         pass
 
     @allure.title('test_payout_order_001')
-    @allure.description('wallet 验证')
+    @allure.description('ETH Payout Order校验')
     def test_payout_order_001(self):
         with allure.step("生成一笔ETH payout订单"):
             transaction_id = ApiFunction.get_payout_transaction_id(amount='0.01', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='ETH')
@@ -38,9 +38,61 @@ class TestAccountingPayOutOrderApi:
             assert movement_id_service_charge != (), '确认 PAYOUT_TXN_STATUS_EXECUTING 手续费失败'
             assert len(movement_id_service_charge) == 2, '手续费一借一贷错误'
 
+    @allure.title('test_payout_order_002')
+    @allure.description('USDT Payout Order校验')
+    def test_payout_order_002(self):
+        with allure.step("生成一笔USDT payout订单"):
+            transaction_id = ApiFunction.get_payout_transaction_id(amount='7.1', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='USDT')
+            sleep(10)
+        with allure.step("确认 PAYOUT_TXN_STATUS_CREATED 冻账"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_CREATED';".format(transaction_id)
+            movement_id_create = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_create != (), 'PAYOUT_TXN_STATUS_CREATED冻账失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 内部户转出"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_EXECUTING' and offset = 1;".format(transaction_id)
+            movement_id = sqlFunction().connect_mysql('wallet', sql=sql)
+            sql = "select * from internal_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_internal_balance = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_internal_balance != (), 'PAYOUT_TXN_STATUS_EXECUTING 内部户转出失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 外部户收到"):
+            sql = "select * from client_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_client_balance = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_client_balance != (), 'PAYOUT_TXN_STATUS_EXECUTING 外部户收到失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 手续费"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_EXECUTING' and offset = 2;".format(transaction_id)
+            movement_id = sqlFunction().connect_mysql('wallet', sql=sql)
+            sql = "select * from internal_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_service_charge = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_service_charge != (), '确认 PAYOUT_TXN_STATUS_EXECUTING 手续费失败'
+            assert len(movement_id_service_charge) == 2, '手续费一借一贷错误'
 
-
-
+    @allure.title('test_payout_order_003')
+    @allure.description('BTC Payout Order校验')
+    def test_payout_order_003(self):
+        with allure.step("生成一笔BTC payout订单"):
+            transaction_id = ApiFunction.get_payout_transaction_id(amount='0.005', address='0xf48e06660E4d3D7Cf89B6977463379bcCD5c0d1C', code_type='BTC')
+            sleep(10)
+        with allure.step("确认 PAYOUT_TXN_STATUS_CREATED 冻账"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_CREATED';".format(transaction_id)
+            movement_id_create = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_create != (), 'PAYOUT_TXN_STATUS_CREATED冻账失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 内部户转出"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_EXECUTING' and offset = 1;".format(transaction_id)
+            movement_id = sqlFunction().connect_mysql('wallet', sql=sql)
+            sql = "select * from internal_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_internal_balance = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_internal_balance != (), 'PAYOUT_TXN_STATUS_EXECUTING 内部户转出失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 外部户收到"):
+            sql = "select * from client_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_client_balance = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_client_balance != (), 'PAYOUT_TXN_STATUS_EXECUTING 外部户收到失败'
+        with allure.step("确认 PAYOUT_TXN_STATUS_EXECUTING 手续费"):
+            sql = "select movement_id from movement where transaction_id = '{}' and memo = 'PAYOUT_TXN_STATUS_EXECUTING' and offset = 2;".format(transaction_id)
+            movement_id = sqlFunction().connect_mysql('wallet', sql=sql)
+            sql = "select * from internal_balance where movement_id = '{}';".format(movement_id[0]['movement_id'])
+            movement_id_service_charge = sqlFunction().connect_mysql('wallet', sql=sql)
+            assert movement_id_service_charge != (), '确认 PAYOUT_TXN_STATUS_EXECUTING 手续费失败'
+            assert len(movement_id_service_charge) == 2, '手续费一借一贷错误'
 
 
 
