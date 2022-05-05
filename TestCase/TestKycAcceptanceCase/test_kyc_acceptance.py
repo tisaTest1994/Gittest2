d01463a692e043353d5c5c082c41f1ac564f17aa
Richard Wan <richard.wan@cabital.com>
2022-05-05 18:47:08 +0800

    test all

--- TestCase/TestKycAcceptanceCase/test_kyc_acceptance.py
+++ TestCase/TestKycAcceptanceCase/test_kyc_acceptance.py
@@ -1,13 +1,8 @@
 from Function.api_function import *
 from Function.operate_sql import *
-import pytest
 
 
 # kyc acceptance相关cases
-class TestKycAcceptanceApi:
+#class TestKycAcceptanceApi:
 
-    @pytest.mark.flaky(reruns=5, reruns_delay=1)
-    def test_e3g_mgt_001(self):
-        a = random.random()
-        print(a)
-        assert a > 0.9, 'error a={}'.format(a)
+
