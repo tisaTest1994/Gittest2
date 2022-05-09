76b5cce83cf13088e419270959bccc396b8b15b5
Richard Wan <richard.wan@cabital.com>
2022-05-09 16:32:02 +0800

    123123

--- TestCase/TestAccountingCase/test_wallet.py
+++ TestCase/TestAccountingCase/test_wallet.py
@@ -40,8 +40,6 @@ class TestAccountingWalletApi:
                                     wallet_subType = str(line_info[8]).split('(')[0].split(":'")[1]
                                 else:
                                     wallet_subType = str(line_info[8]).split("'")[1]
-                            print(str(line_info[3]).split(':')[1])
-                            print(str(line_info[9]).split(':')[1])
                             sql = "select * from wallet where wallet_name = {} and code = {} and account_code = {} and status = {} and allow_overdraft = {} and balance_direction = {} and wallet_type = '{}';".format(
                                 str(line_info[3]).split(':')[1], str(line_info[5]).split(':')[1],
                                 int(float(str(line_info[9]).split(':')[1])), status, allow_overdraft, balance_direction,
