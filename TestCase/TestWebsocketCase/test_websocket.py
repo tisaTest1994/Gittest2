e9d235a5ed30498bf7d57f2e8339723865a9145c
Richard Wan <richard.wan@cabital.com>
2022-05-23 14:43:14 +0800

    1

--- TestCase/TestWebsocketCase/test_websocket.py
+++ TestCase/TestWebsocketCase/test_websocket.py
@@ -15,7 +15,7 @@ class TestCoreApi:
     @allure.title('test_websocket_001')
     @allure.description('通过websocket订阅')
     def test_websocket_001(self):
-        for i in get_json()['cfx_book'].values():
+        for i in ApiFunction.get_cfx_list():
             pair = i.split('-')
             asyncio.get_event_loop().run_until_complete(main_logic(params=["quote@{}{}".format(pair[0], pair[1])]))
             sleep(30)
