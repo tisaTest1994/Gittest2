40eb6fb7324e8d431dc14d18b92532a1c68f674f
Richard Wan <richard.wan@cabital.com>
2022-05-26 11:33:47 +0800

    no message

--- TestCase/TestInfinniGamesCase/test_account.py
+++ TestCase/TestInfinniGamesCase/test_account.py
@@ -5,4 +5,20 @@ from Function.operate_sql import *
 # Account相关cases
 class TestAccountApi:
 
-    url = get_json()['connect'][get_json()['env']]['url']
+    @allure.title('test_account_001')
+    @allure.description('注册新用户,用户未被注册')
+    def test_account_001(self):
+        with allure.step("注册账户"):
+            headers['X-Device'] = ''
+            data = {
+                "emailAddress": generate_email(),
+                "verificationCode": "666666",
+                "password": get_json()['email']['password']
+            }
+            r = session.request('POST', url='{}/account/user/signUp'.format(env_url), data=json.dumps(data), headers=headers)
+        with allure.step("状态码和返回值"):
+            logger.info('状态码是{}'.format(str(r.status_code)))
+            logger.info('返回值是{}'.format(str(r.text)))
+        with allure.step("校验状态码"):
+            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+        with allure.step("校验返回值"):
