6ab34011ee84f84f63f77c368d8c7ab59bb0c18e
Richard Wan <richard.wan@cabital.com>
2022-06-02 10:24:49 +0800

    test all

--- TestCase/TestInfinniGamesCase/test_account.py
+++ TestCase/TestInfinniGamesCase/test_account.py
@@ -1,5 +1,3 @@
-import json
-
 from Function.api_function import *
 from Function.operate_sql import *
 
@@ -18,75 +16,74 @@ class TestAccountApi:
     @allure.title('test_account_001')
     @allure.description('链接新用户')
     def test_account_001(self):
-        with allure.step("获得用户信息"):
+        print('等待第二期api unlink')
+        # with allure.step("获得用户信息"):
+        #     params = {
+        #         'user_ext_ref': get_json()['infinni_games']['uid_A'],
+        #         'partner_key': '07c9297b-65f1-4e16-a0bd-ff6889e386de'
+        #     }
+        #     with allure.step("验签"):
+        #         sign = ApiFunction.infinni_games_access_sign(url='{}/partner/link?user_ext_ref={}&partner_key={}'.format(self.url, params['user_ext_ref'], params['partner_key']))
+        #     params['signature'] = sign
+        #     r = session.request('GET', url='{}/partner/link'.format(self.url), params=params, headers=headers)
+        #     with allure.step("校验状态码"):
+        #         assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+        # with allure.step("注册账户"):
+        #     data = {
+        #         'user_ext_ref': params['user_ext_ref']
+        #     }
+        #     r = session.request('POST', url='{}/connect/account/{}/link'.format(env_url,
+        #                                                                         get_json()['infinni_games'][
+        #                                                                             'partner_id']),
+        #                         data=json.dumps(data), headers=headers)
+        #     with allure.step("校验状态码"):
+        #         assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+        #     with allure.step("校验返回值"):
+        #         assert r.json()['link_status'] == 2, "link新用户失败，返回值是{}".format(r.text)
+        #         assert r.json()['connect_status'] == 3, "link新用户失败，返回值是{}".format(r.text)
+        # with allure.step("删除数据库link信息"):
+        #     sql = "delete from account_link where user_ref_id = '{}';".format(params['user_ext_ref'])
+        #     logger.info('sql命令是{}'.format(sql))
+        #     sqlFunction().connect_mysql('partner', sql=sql)
+
+    @allure.title('test_account_002')
+    @allure.description('获取cabital账号link infinni games账号')
+    def test_account_002(self):
+        with allure.step("获取cabital账号link infinni games账号"):
             params = {
-                'user_ext_ref': get_json()['infinni_games']['uid_A'],
-                'partner_key': '07c9297b-65f1-4e16-a0bd-ff6889e386de'
+                'partner_ids': get_json()['infinni_games']['partner_id']
             }
-            with allure.step("验签"):
-                sign = ApiFunction.infinni_games_access_sign(url='{}/partner/link?user_ext_ref={}&partner_key={}'.format(self.url, params['user_ext_ref'], params['partner_key']))
-            params['signature'] = sign
-            r = session.request('GET', url='{}/partner/link'.format(self.url), params=params, headers=headers)
-            with allure.step("校验状态码"):
-                assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-        with allure.step("注册账户"):
-            data = {
-                'user_ext_ref': params['user_ext_ref']
-            }
-            r = session.request('POST', url='{}/connect/account/{}/link'.format(env_url,
-                                                                                get_json()['infinni_games'][
-                                                                                    'partner_id']),
-                                data=json.dumps(data), headers=headers)
+            r = session.request('GET', url='{}/connect/account/links'.format(self.url), params=params, headers=headers)
             with allure.step("校验状态码"):
                 assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
             with allure.step("校验返回值"):
-                assert r.json()['link_status'] == 2, "link新用户失败，返回值是{}".format(r.text)
-                assert r.json()['connect_status'] == 3, "link新用户失败，返回值是{}".format(r.text)
-        with allure.step("删除数据库link信息"):
-            sql = "delete from account_link where user_ref_id = '{}';".format(params['user_ext_ref'])
-            logger.info('sql命令是{}'.format(sql))
-            sqlFunction().connect_mysql('partner', sql=sql)
 
+                assert r.json()['list'][0]['name'] == 'Yeeha Games', "获取cabital账号link infinni games账号错误，返回值是{}".format(r.text)
 
+    @allure.title('test_account_003')
+    @allure.description('获取cabital账号link infinni games账号 旧接口')
+    def test_account_003(self):
+        with allure.step("获取cabital账号link infinni games账号"):
+            params = {
+                'partner_id': get_json()['infinni_games']['partner_id']
+            }
+            r = session.request('GET', url='{}/connect/account/info'.format(self.url), params=params, headers=headers)
+            with allure.step("校验状态码"):
+                assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+            with allure.step("校验返回值"):
+                assert r.json()['account_bindings'][0]['partner_name'] == 'Yeeha Games', "获取cabital账号link infinni games账号错误，返回值是{}".format(r.text)
 
+    @allure.title('test_account_004')
+    @allure.description('获取infinni games账号信息')
+    def test_account_004(self):
+        with allure.step("获取infinni games账号信息"):
+            params = {
+                'partner_id': get_json()['infinni_games']['partner_id'],
+                'user_ext_ref': get_json()['infinni_games']['uid_A']
 
-
-
-    #     with allure.step("校验状态码"):
-    #         assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-    #     with allure.step("校验返回值"):
-    #         print(r.json())
-    #         assert r.json()['connect_status'] == 3, "用户连接失败，返回值是{}".format(r.text)
-    #         account_vid = r.json()['account_vid']
-    #     with allure.step("断开链接unlink"):
-    #         data = {
-    #             'account_vid': account_vid
-    #         }
-    #         r = session.request('POST',
-    #                             url='{}/connect/account/{}/unlink'.format(env_url,
-    #                                                                       get_json()['infinni_games']['partner_id']),
-    #                             data=json.dumps(data), headers=headers)
-    #         with allure.step("校验状态码"):
-    #             assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-    #
-    # @allure.title('test_account_002')
-    # @allure.description('链接新用户')
-    # def test_account_002(self):
-    #     params = {
-    #         'user_ext_ref': ''
-    #     }
-    #     r = session.request('GET',
-    #                         url='{}/partner/link?user_ext_ref={}&partner_key=07c9297b-65f1-4e16-a0bd-ff6889e386de'.format(env_url,
-    #                                                                   get_json()['infinni_games']['partner_id']), headers=headers)
-    #     print(r.url)
-    #     print(r.json())
-    #
-    #     data = {
-    #         'account_vid': 'f21246e7-e849-4230-9f23-2682bc8a886d'
-    #     }
-    #     r = session.request('POST',
-    #                         url='{}/connect/account/{}/unlink'.format(env_url, get_json()['infinni_games']['partner_id']),
-    #                         data=json.dumps(data), headers=headers)
-    #     with allure.step("状态码和返回值"):
-    #         logger.info('状态码是{}'.format(str(r.status_code)))
-    #         logger.info('返回值是{}'.format(str(r.text)))
+            }
+            r = session.request('GET', url='{}/connect/partner/user/info'.format(self.url), params=params, headers=headers)
+            with allure.step("校验状态码"):
+                assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+            with allure.step("校验返回值"):
+                assert r.json()['email'] == 'zcdsw@sina.com', "获取infinni games账号信息错误,返回值是{}".format(r.text)
---
+++ TestCase/TestInfinniGamesCase/test_convert.py
@@ -0,0 +1,62 @@
+from Function.api_function import *
+from Function.operate_sql import *
+
+
+# Convert相关cases
+class TestConvertApi:
+    url = get_json()['infinni_games']['url']
+
+    # 初始化class
+    def setup_method(self):
+        with allure.step("登录客户账户获得后续操作需要的token"):
+            ApiFunction.add_headers()
+        with allure.step("多语言支持"):
+            headers['locale'] = 'zh-TW'
+
+    @allure.title('test_partner_config')
+    @allure.description('账户操作相关')
+    def test_partner_config_001(self):
+        with allure.step("验签"):
+            unix_time = int(time.time())
+            nonce = generate_string(30)
+            sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='GET', url='/api/v1/config',
+                                                nonce=nonce)
+            connect_headers['ACCESS-SIGN'] = sign
+            connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+            connect_headers['ACCESS-NONCE'] = nonce
+        with allure.step("获取合作方的配置"):
+            r = session.request('GET', url='{}/config'.format(self.url), headers=connect_headers)
+        with allure.step("校验状态码"):
+            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+        with allure.step("校验返回值"):
+            assert r.json()['currencies'] is not None
+
+    @allure.title('test_currency_quotes_001')
+    @allure.description('获取报价最新的报价')
+    def test_currency_quotes_001(self):
+        with allure.step("获取最新的报价"):
+            for i in get_json()['cfx_book'].values():
+                with allure.step("获取正向报价"):
+                    r = session.request('GET', url='{}/quotes/{}'.format(self.url, i), headers=connect_headers)
+                    logger.info('币种对为{}'.format(i))
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+                with allure.step("获取反向报价"):
+                    new_pair = '{}{}{}'.format(i.split('-')[1], '-', i.split('-')[0])
+                    r = session.request('GET', url='{}/quotes/{}'.format(self.url, new_pair),
+                                        headers=connect_headers)
+                    logger.info('币种对为{}'.format(new_pair))
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+
+
