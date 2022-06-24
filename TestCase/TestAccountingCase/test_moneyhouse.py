96457112861f37b619216167a095d8f46c8b91b7
Richard Wan <richard.wan@cabital.com>
2022-06-24 11:43:42 +0800

    1123

--- TestCase/TestAccountingCase/test_moneyhouse.py
+++ TestCase/TestAccountingCase/test_moneyhouse.py
@@ -39,18 +39,10 @@ class TestMoneyHouseApi:
         sheet_name = 'Crypto'
         for i in range(1, OperateExcel.get_excel_sheet_all_row_number(sheet_name, path=self.money_house_path)):
             line_info = OperateExcel.get_excel_sheet_row(sheet_name, i, path=self.money_house_path)
-            if str(line_info[3]).split(':')[1] == 'CA':
-                type = 1
-            elif str(line_info[3]).split(':')[1] == 'MP':
-                type = 2
-            elif str(line_info[3]).split(':')[1] == 'COST':
-                type = 3
-            else:
-                type = 0
             sql = "select money_house_id from money_house where id = (select id from moneyhouse.money_house_account where nick_name = {});".format(str(line_info[4]).split(':')[1])
+            print(11111111111)
             print(sql)
             info = sqlFunction().connect_mysql('moneyhouse', sql=sql)
             print(info)
-            print(type(info))
 
 
