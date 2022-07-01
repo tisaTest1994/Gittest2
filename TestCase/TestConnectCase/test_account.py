dc21bc3117073bcc7f746c9ccb8550a7748b467a
alice <alice.wang@cabital.com>
2022-07-01 15:20:52 +0800

    update

--- TestCase/TestConnectCase/test_account.py
+++ TestCase/TestConnectCase/test_account.py
@@ -57,7 +57,7 @@ class TestAccountApi:
             logger.info('返回值是{}'.format(str(r.text)))
         with allure.step("校验状态码"):
             if expect_status == "NONE":
-                assert r.status_code == 403, "http状态码不对，目前状态码是{}".format(r.status_code)
+                assert r.status_code == 400, "http状态码不对，目前状态码是{}".format(r.status_code)
             else:
                 assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
