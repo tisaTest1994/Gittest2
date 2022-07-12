885e8a35c70108ebb6067bdd665125b68cc21985
alice <alice.wang@cabital.com>
2022-07-12 17:34:04 +0800

    提交

--- TestCase/TestInfinniGamesCase/test_transfer.py
+++ TestCase/TestInfinniGamesCase/test_transfer.py
@@ -135,59 +135,71 @@ class TestTransferApi:
             assert r.json()['user_ext_ref'] == 'weqw@163.com', '基于划转ID获取划转详情错误，返回值是{}'.format(r.text)
 
     @allure.title('test_transfer_007')
-    @allure.description('infinni games申请，把资金从cabital划转到infinni games')
+    @allure.description('infinni games申请direct debit，把资金从cabital划转到infinni games')
     def test_transfer_007(self):
         with allure.step("测试用户的account_id"):
             account_id = get_json()['infinni_games']['account_vid']
         with allure.step("获得otp"):
             mfaVerificationCode = get_mfa_code(get_json()['email']['secretKey_richard'])
         with allure.step("获得data"):
+            symbol = 'USDT'
             external_id = generate_string(25)
+            debit_limit = ApiFunction.get_config_info(project='infinni games', type='debit_limit', symbol=symbol)
+            logger.info('debit_limit最小限额值是：{}'.format(debit_limit[0][0]))
+            debit_fee_value = ApiFunction.get_config_info(project='infinni games', type='debit_fee', symbol=symbol)
+            logger.info('debit_fee_value优惠值是：{}，fee原价是：{}'.format(debit_fee_value[0][0], debit_fee_value[0][1]))
+            amount = int(debit_limit[0][0]) + int(10) - int(debit_fee_value[0][0])
+            logger.info('amount传入值是：{}'.format(amount))
             data = {
-                'amount': '120',
-                'symbol': 'USDT',
+                'amount': str(amount),
+                'symbol': symbol,
                 'otp': str(mfaVerificationCode),
                 'direction': 'DEBIT',
                 'external_id': external_id
             }
-        with allure.step("验签"):
-            unix_time = int(time.time())
-            nonce = generate_string(30)
-            sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='POST',
-                                                url='/api/v1/accounts/{}/transfers'.format(account_id),
-                                                key='infinni games', nonce=nonce, body=json.dumps(data))
-            headers['ACCESS-SIGN'] = sign
-            headers['ACCESS-TIMESTAMP'] = str(unix_time)
-            headers['ACCESS-NONCE'] = nonce
-        with allure.step('获得transfer前币种可用balance数量'):
-            transfer_amount_wallet_balance_old = ApiFunction.get_crypto_number(type=data['symbol'])
-        with allure.step("transfer"):
-            r = session.request('POST', url='{}/accounts/{}/transfers'.format(self.url, account_id),
-                                data=json.dumps(data), headers=headers)
-            logger.info('r.json返回值是{}'.format(r.json()))
-        with allure.step('获得transfer后币种balance数量'):
-            transfer_amount_wallet_balance_latest = ApiFunction.get_crypto_number(type=data['symbol'])
-        with allure.step("校验状态码"):
-            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-        with allure.step("校验返回值"):
-            assert r.json()['transfer_id'] is not None, '划转失败，返回值是{}'.format(r.text)
-        with allure.step("校验transfer前后的可用balance数量"):
-            assert Decimal(transfer_amount_wallet_balance_old) - Decimal(data['amount']) == Decimal(
-                transfer_amount_wallet_balance_latest), "transfer前后可用balance数量不对，transfer前balance是{}，transfer后balance是{}".format(
-                transfer_amount_wallet_balance_old, transfer_amount_wallet_balance_latest)
+            with allure.step("验签"):
+                unix_time = int(time.time())
+                nonce = generate_string(30)
+                sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='POST',
+                                                    url='/api/v1/accounts/{}/transfers'.format(account_id),
+                                                    key='infinni games', nonce=nonce, body=json.dumps(data))
+                headers['ACCESS-SIGN'] = sign
+                headers['ACCESS-TIMESTAMP'] = str(unix_time)
+                headers['ACCESS-NONCE'] = nonce
+            with allure.step('获得transfer前币种可用balance数量'):
+                transfer_amount_wallet_balance_old = ApiFunction.get_crypto_number(type=data['symbol'])
+            with allure.step("transfer"):
+                r = session.request('POST', url='{}/accounts/{}/transfers'.format(self.url, account_id),
+                                    data=json.dumps(data), headers=headers)
+                logger.info('r.json返回值是{}'.format(r.json()))
+            with allure.step('获得transfer后币种balance数量'):
+                transfer_amount_wallet_balance_latest = ApiFunction.get_crypto_number(type=data['symbol'])
+            with allure.step("校验状态码"):
+                assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+            with allure.step("校验返回值"):
+                assert r.json()['transfer_id'] is not None, "划转失败，返回值是{}".format(r.text)
+            with allure.step("校验transfer前后的可用balance数量"):
+                assert Decimal(transfer_amount_wallet_balance_old) - Decimal(data['amount']) == Decimal(
+                    transfer_amount_wallet_balance_latest), "transfer前后可用balance数量不对，transfer前balance是{}，transfer后balance是{}".format(
+                    transfer_amount_wallet_balance_old, transfer_amount_wallet_balance_latest)
 
     @allure.title('test_transfer_008')
-    @allure.description('infinni games申请，把资金从infinni games划转到cabital')
+    @allure.description('infinni games申请发起一笔direct credit，把资金从infinni games划转到cabital')
     def test_transfer_008(self):
         with allure.step("测试用户的account_id"):
             account_id = get_json()['infinni_games']['account_vid']
         with allure.step("获得otp"):
             mfaVerificationCode = get_mfa_code(get_json()['email']['secretKey_richard'])
         with allure.step("获得data"):
+            symbol = 'USDT'
             external_id = generate_string(25)
+            credit_limit = ApiFunction.get_config_info(project='infinni games', type='credit_limit', symbol=symbol)
+            logger.info('credit_limit最小限额值是：{}'.format(credit_limit[0][0]))
+            amount = int(credit_limit[0][0])
+            logger.info('amount传入值是：{}'.format(amount))
             data = {
-                'amount': '100',
-                'symbol': 'USDT',
+                'amount': str(amount),
+                'symbol': symbol,
                 'otp': str(mfaVerificationCode),
                 'direction': 'CREDIT',
                 'external_id': external_id
@@ -223,18 +235,21 @@ class TestTransferApi:
     def test_transfer_009(self):
         with allure.step("测试用户的account_id"):
             account_id = get_json()['infinni_games']['account_vid']
-        for i in ApiFunction.get_config_info(type='pairs'):
-            with allure.step('换汇'):
-                transaction = ApiFunction.cfx_random(i, i.split('-')[0])
-                cfx_transaction_id = transaction['returnJson']['transaction']['transaction_id']
-                with allure.step("获得data"):
-                    external_id = generate_string(25)
-                    if i.split('-')[0] in get_json()['crypto_list']:
-                        symbol = i.split('-')[0]
-                    else:
-                        symbol = i.split('-')[1]
+        with allure.step("获得data"):
+            symbol = 'USDT'
+            external_id = generate_string(25)
+            cfx_limit = ApiFunction.get_config_info(project='infinni games', type='cfx_limit', symbol=symbol)
+            logger.info('cfx_limit最小限额值是：{}'.format(cfx_limit[0][0]))
+            buy_amount = int(cfx_limit[0][0])
+            logger.info('buy_amount值是：{}'.format(buy_amount))
+        for i in ApiFunction.get_config_info(project='infinni games', type='pairs'):
+            if i.split('-')[0] == 'USDT':
+                with allure.step('换汇'):
+                    transaction = ApiFunction.cfx_random(i, i.split('-')[0], buy_amount=buy_amount)
+                    logger.info('换汇后transaction的返回值是{}'.format(transaction))
+                    cfx_transaction_id = transaction['returnJson']['transaction']['transaction_id']
                     data = {
-                        'amount': transaction['data']['buy_amount'],
+                        'amount': str(buy_amount),
                         'symbol': symbol,
                         'otp': get_mfa_code(get_json()['email']['secretKey_richard']),
                         'conversion_id': cfx_transaction_id,
@@ -294,7 +309,6 @@ class TestTransferApi:
             assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验transfer前后的可用balance数量"):
             assert Decimal(transfer_amount_wallet_balance_old) - Decimal(data['amount']) == Decimal(transfer_amount_wallet_balance_latest), "transfer前后可用balance数量不对，transfer前balance是{}，transfer后balance是{}".format(transfer_amount_wallet_balance_old, transfer_amount_wallet_balance_latest)
-            sleep(30)
         with allure.step("获得otp"):
             mfaVerificationCode = get_mfa_code(get_json()['email']['secretKey_richard'])
         with allure.step("获得data"):
@@ -331,13 +345,13 @@ class TestTransferApi:
             assert Decimal(transfer_amount_wallet_balance_latest2) == Decimal(
                 transfer_amount_wallet_balance_old2) + Decimal(amount1), "transfer前后可用balance数量不对，transfer前balance是{}，transfer后balance是{}".format(
                 transfer_amount_wallet_balance_old2, transfer_amount_wallet_balance_latest2)
-        with allure.step("校验2笔transfer前后的可用balance数量"):
+        with allure.step("总校验2笔transfer前后的可用balance数量"):
             assert Decimal(transfer_amount_wallet_balance_old) - Decimal(amount) == Decimal(
                 transfer_amount_wallet_balance_latest2) - Decimal(amount1), "transfer前后可用balance数量不对，transfer前balance是{}，transfer后balance是{}".format(
                 transfer_amount_wallet_balance_old, transfer_amount_wallet_balance_latest2)
 
     @allure.title('test_transfer_011')
-    @allure.description('对账 - 划转交易详情使用external_id')
+    @allure.description('对账 - 划转交易详情使用正确的external_id')
     def test_transfer_011(self):
         external_id = 'fVug0yqnyCJcPRbpgYnilfUPF'
         with allure.step("验签"):
@@ -359,42 +373,5 @@ class TestTransferApi:
         with allure.step("校验返回值"):
             assert r.json()['external_id'] == external_id, "对账 - 划转交易详情使用external_id错误，返回值是{}".format(r.text)
 
-    @allure.title('test_transfer_012')
-    @allure.description('infinni games申请，先做C+T，单做一笔交易C成功T失败')
-    def test_transfer_012(self):
-        with allure.step("测试用户的account_id"):
-            account_id = get_json()['infinni_games']['account_vid']
-            with allure.step('换汇'):
-                pair = 'USDT-EUR'
-                transaction = ApiFunction.cfx_random(pair, pair.split('-')[0])
-                cfx_transaction_id = transaction['returnJson']['transaction']['transaction_id']
-                with allure.step("获得data"):
-                    external_id = generate_string(25)
-                    data = {
-                        'amount': '10',
-                        'symbol': 'USDT',
-                        'otp': '123456',
-                        'conversion_id': cfx_transaction_id,
-                        'direction': 'DEBIT',
-                        'external_id': external_id
-                    }
-                with allure.step("验签"):
-                    unix_time = int(time.time())
-                    nonce = generate_string(30)
-                    sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='POST',
-                                                        url='/api/v1/accounts/{}/transfers'.format(account_id),
-                                                        key='infinni games', nonce=nonce, body=json.dumps(data))
-                    headers['ACCESS-SIGN'] = sign
-                    headers['ACCESS-TIMESTAMP'] = str(unix_time)
-                    headers['ACCESS-NONCE'] = nonce
-                with allure.step("transfer"):
-                    r = session.request('POST', url='{}/accounts/{}/transfers'.format(self.url, account_id),
-                                        data=json.dumps(data), headers=headers)
-                    logger.info('r.json()的返回值是{}'.format(r.json()))
-                    logger.info('conversion_id的值是{}'.format(data['conversion_id']))
-                with allure.step("校验状态码"):
-                    assert r.status_code == 400, "http状态码不对，目前状态码是{}".format(r.status_code)
-                with allure.step("校验返回值"):
-                    assert r.json()['message'] == 'mfa failed', '划转失败，返回值是{}'.format(r.text)
-                with allure.step("等待30秒"):
-                    sleep(30)
+
+
