23e1ed21dafd63e459b8ae3a001d84456c385667
Richard Wan <richard.wan@cabital.com>
2022-07-22 18:52:21 +0800

    123123

--- TestCase/TestAccountingCase/test_quote.py
+++ TestCase/TestAccountingCase/test_quote.py
@@ -11,7 +11,6 @@ class TestQuoteApi:
         with allure.step("登录客户账户获得后续操作需要的token"):
             ApiFunction.add_headers()
 
-
     @allure.title('test_quote_001')
     @allure.description('校验cfx汇率增加的浮点')
     def test_quote_001(self):
@@ -61,9 +60,9 @@ class TestQuoteApi:
                 logger.info('汇率list是{}'.format(quote))
                 service_charge_type = 0
                 with allure.step("获取需要增加的汇率"):
-                    for y in (get_json()['cfx_service_charge']).keys():
+                    for y in (get_json()['check_out_cfx_service_charge']).keys():
                         if y in i:
-                            service_charge = get_json()['cfx_service_charge'][y]
+                            service_charge = get_json()['check_out_cfx_service_charge'][y]
                             assert (Decimal(quote['original_bid']) * Decimal(str(1 - service_charge))).quantize(Decimal('0.00'), ROUND_FLOOR) == Decimal(quote['bid']).quantize(Decimal('0.00'), ROUND_FLOOR), '校验cfx汇率增加的浮点错误，original_bid是{}, bid是{}, pair是{}'.format(quote['original_bid'], quote['bid'], i)
                             assert (Decimal(quote['original_ask']) * Decimal(str(1 + service_charge))).quantize(
                                 Decimal('0.00'), ROUND_FLOOR) == Decimal(quote['ask']).quantize(Decimal('0.00'),
@@ -71,7 +70,7 @@ class TestQuoteApi:
                                 quote['original_ask'], quote['ask'], i)
                             service_charge_type = 1
                     if service_charge_type == 0:
-                        service_charge = get_json()['cfx_service_charge']['Other']
+                        service_charge = get_json()['check_out_cfx_service_charge']['Other']
                         assert (Decimal(quote['original_bid']) * Decimal(str(1 - service_charge))).quantize(
                             Decimal('0.00'),
                             ROUND_FLOOR) == Decimal(
