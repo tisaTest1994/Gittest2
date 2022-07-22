92bd42a873c7353c13f1b08fd31640aafb9758e0
Richard Wan <richard.wan@cabital.com>
2022-07-22 14:25:46 +0800

    fix bug

--- TestCase/TestAccountingCase/test_quote.py
+++ TestCase/TestAccountingCase/test_quote.py
@@ -20,13 +20,14 @@ class TestQuoteApi:
             for i in cfx_list:
                 sql = "select bid, ask, original_bid, original_ask from quote_{} where pair = '{}' and purpose = 'Customer' limit 1;".format(day_time, i)
                 logger.info('sql命令是{}'.format(sql))
-                quote = sqlFunction().connect_mysql('pricing', sql=sql)
-                print(quote[0])
-                print(quote[0]['bid'])
-                # with allure.step("获取需要增加的汇率"):
-                #     for y in (get_json()['cfx_service_charge']).keys():
-                #         if y in i:
-                #             print(y)
-                #             print(i)
+                quote = (sqlFunction().connect_mysql('pricing', sql=sql))[0]
+                with allure.step("获取需要增加的汇率"):
+                    for y in (get_json()['cfx_service_charge']).keys():
+                        if y in i:
+                            service_charge = get_json()['cfx_service_charge'][y]
+                            assert (Decimal(quote['original_bid']) * Decimal(1 - service_charge)).quantize(Decimal('0.00'), ROUND_FLOOR) == Decimal(quote['bid']).quantize(Decimal('0.00'), ROUND_FLOOR), '校验cfx汇率增加的浮点错误，original_bid是{}, bid是{}, pair是{}'.format(quote['original_bid'], quote['bid'], i)
+                        else:
+                            service_charge = get_json()['cfx_service_charge']['Other']
+                            assert (Decimal(quote['original_bid']) * Decimal(1 - service_charge)).quantize(Decimal('0.00'), ROUND_FLOOR) == Decimal(quote['bid']).quantize(Decimal('0.00'), ROUND_FLOOR), '校验cfx汇率增加的浮点错误，original_bid是{}, bid是{}, pair是{}'.format(quote['original_bid'], quote['bid'], i)
 
 
