6c4b2800b5e084b1ad95768ad318726ebc20b517
alice <alice.wang@cabital.com>
2022-08-25 18:25:51 +0800

    update

--- TestCase/TestConnectCase/test_account.py
+++ TestCase/TestConnectCase/test_account.py
@@ -120,41 +120,41 @@ class TestAccountApi:
         with allure.step("校验返回值"):
             assert r.json()['otp_ready'] is True, "查询用户otp状态，otp已经绑定错误，返回值是{}".format(r.text)
 
-    @allure.title('test_connect_account_004')
-    @allure.description('成功解绑+name match用户 pass')
-    def test_connect_account_04(self):
-        with allure.step("准备参数"):
-            account_id = 'cd7e353b-6f4c-45db-bdd5-78bdc13a53c7'
-        with allure.step("name match 数据"):
-            data = {
-                'name': 'Neo DingTest6',
-                'id': '356214563',
-                'id_document': 'PASSPORT',
-                'issued_by': 'HKG',
-                'dob': '19910101'
-            }
-        with allure.step("验签"):
-            unix_time = int(time.time())
-            nonce = generate_string(30)
-            sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='PUT', url='/api/v1/accounts/{}/match'.format(account_id), nonce=nonce, body=json.dumps(data))
-            connect_headers['ACCESS-SIGN'] = sign
-            connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
-            connect_headers['ACCESS-NONCE'] = nonce
-        with allure.step("name match"):
-            r = session.request('PUT', url='{}/accounts/{}/match'.format(self.url, account_id), data=json.dumps(data), headers=connect_headers)
-        with allure.step("状态码和返回值"):
-            # logger.info('状态码是{}'.format(str(r.status_code)))
-            logger.info('返回值是{}'.format(str(r.text)))
-        with allure.step("校验状态码"):
-            assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-        with allure.step("校验返回值"):
-            assert r.json()['result'] == 'PASS', "name match pass错误，返回值是{}".format(r.text)
+    # @allure.title('test_connect_account_004')
+    # @allure.description('成功解绑+name match用户 pass')
+    # def test_connect_account_04(self):
+    #     with allure.step("准备参数"):
+    #         account_id = 'cd7e353b-6f4c-45db-bdd5-78bdc13a53c7'
+    #     with allure.step("name match 数据"):
+    #         data = {
+    #             'name': 'Neo DingTest6',
+    #             'id': '356214563',
+    #             'id_document': 'PASSPORT',
+    #             'issued_by': 'HKG',
+    #             'dob': '19910101'
+    #         }
+    #     with allure.step("验签"):
+    #         unix_time = int(time.time())
+    #         nonce = generate_string(30)
+    #         sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='PUT', url='/api/v1/accounts/{}/match'.format(account_id), nonce=nonce, body=json.dumps(data))
+    #         connect_headers['ACCESS-SIGN'] = sign
+    #         connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+    #         connect_headers['ACCESS-NONCE'] = nonce
+    #     with allure.step("name match"):
+    #         r = session.request('PUT', url='{}/accounts/{}/match'.format(self.url, account_id), data=json.dumps(data), headers=connect_headers)
+    #     with allure.step("状态码和返回值"):
+    #         # logger.info('状态码是{}'.format(str(r.status_code)))
+    #         logger.info('返回值是{}'.format(str(r.text)))
+    #     with allure.step("校验状态码"):
+    #         assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+    #     with allure.step("校验返回值"):
+    #         assert r.json()['result'] == 'PASS', "name match pass错误，返回值是{}".format(r.text)
 
     @allure.title('test_connect_account_005')
     @allure.description('获取用户绑定关系新（通过bybit账号获取cabital信息）')
     def test_connect_account_005(self):
         with allure.step("获取cabital账号link bybit账号"):
-            headers['Authorization'] = "Bearer " + ApiFunction.get_account_token(account='neoding1@yandex.com', password='Zcdsw123')
+            headers['Authorization'] = "Bearer " + ApiFunction.get_account_token(account='neoding@yandex.com')
             params = {
                 'partner_ids': get_json()['bybit']['partner_id'],
                 'user_ext_ref': '76c7006eba45a314687861ef73c6970a',
--- TestCase/TestConnectCase/test_mobile_api.py
+++ TestCase/TestConnectCase/test_mobile_api.py
@@ -47,32 +47,60 @@ class TestConnectApi:
                 with allure.step("获得transfer后金额"):
                     wallet_balance_latest = ApiFunction.get_crypto_number(type=i)
                 assert Decimal(wallet_balance_old) - Decimal(data['amount']) == Decimal(wallet_balance_latest), 'transfer币种是{},transfer前金额是{},transfer金额是{}，transfer后金额是{}'.format(i, wallet_balance_old, data['amount'], wallet_balance_latest)
-                with allure.step("确认划转"):
-                    if r.json()['status'] == 1:
-                        external_id = generate_string(25)
-                        data = {
-                            "status": "SUCCESS",
-                            "code": "good",
-                            "message": "ok",
-                            "handle_time": int(time.time()),
-                            "external_id": external_id
-                        }
-                        with allure.step("验签"):
-                            unix_time = int(time.time())
-                            nonce = generate_string(30)
-                            sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='PUT',
-                                                                url='/api/v1/accounts/{}/transfers/{}'.format(
-                                                                    get_json()['email']['accountId'],
-                                                                    transfer_id), nonce=nonce, body=json.dumps(data))
-                            connect_headers['ACCESS-SIGN'] = sign
-                            connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
-                            connect_headers['ACCESS-NONCE'] = nonce
-                        r = session.request('PUT', url='{}/accounts/{}/transfers/{}'.format(self.url, get_json()['email']['accountId'], transfer_id), data=json.dumps(data), headers=connect_headers)
-                        with allure.step("校验状态码"):
-                            assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
-                        with allure.step("校验返回值"):
-                            assert r.json() == {}, "确认划转交易错误，返回值是{}".format(r.text)
+                # with allure.step("确认划转"):
+                #     if r.json()['status'] == 1:
+                #         external_id = generate_string(25)
+                #         data = {
+                #             "status": "SUCCESS",
+                #             "code": "good",
+                #             "message": "ok",
+                #             "handle_time": int(time.time()),
+                #             "external_id": external_id
+                #         }
+                #         with allure.step("验签"):
+                #             unix_time = int(time.time())
+                #             nonce = generate_string(30)
+                #             sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='PUT',
+                #                                                 url='/api/v1/accounts/{}/transfers/{}'.format(
+                #                                                     get_json()['email']['accountId'],
+                #                                                     transfer_id), nonce=nonce, body=json.dumps(data))
+                #             connect_headers['ACCESS-SIGN'] = sign
+                #             connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+                #             connect_headers['ACCESS-NONCE'] = nonce
+                #         r = session.request('PUT', url='{}/accounts/{}/transfers/{}'.format(self.url, get_json()['email']['accountId'], transfer_id), data=json.dumps(data), headers=connect_headers)
+                #         with allure.step("校验状态码"):
+                #             assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+                #         with allure.step("校验返回值"):
+                #             assert r.json() == {}, "确认划转交易错误，返回值是{}".format(r.text)
 
-
+    @allure.title('test_connect_001 transfer 交易')
+    @allure.description('transfer 交易')
+    def test_connect_002(self):
+        with allure.step("确认划转"):
+            account_id = 'cd7e353b-6f4c-45db-bdd5-78bdc13a53c7'
+            external_id = generate_string(25)
+            transfer_id = '5e2c1c61-2b57-4732-9aed-f41fffc185be'
+            data = {
+                "status": "FAILED",
+                "code": "bad",
+                "message": "fail error",
+                "handle_time": int(time.time()),
+                "external_id": external_id
+            }
+            with allure.step("验签"):
+                unix_time = int(time.time())
+                nonce = generate_string(30)
+                sign = ApiFunction.make_access_sign(unix_time=str(unix_time), method='PUT',
+                                                    url='/api/v1/accounts/{}/transfers/{}'.format(
+                                                        account_id,
+                                                        transfer_id), nonce=nonce, body=json.dumps(data))
+                connect_headers['ACCESS-SIGN'] = sign
+                connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+                connect_headers['ACCESS-NONCE'] = nonce
+            r = session.request('PUT', url='{}/accounts/{}/transfers/{}'.format(self.url, account_id, transfer_id), data=json.dumps(data), headers=connect_headers)
+            with allure.step("校验状态码"):
+                assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+            with allure.step("校验返回值"):
+                assert r.json() == {}, "确认划转交易错误，返回值是{}".format(r.text)
 
 
