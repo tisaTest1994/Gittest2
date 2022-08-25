18032aa72ec1e04ed0c6e3dcdce648dcda57df4d
Yanting <yanting.huang@cabital.com>
2022-08-25 18:36:10 +0800

    update

--- TestCase/TestApiCase/test_payout_crypto_noraml.py
+++ TestCase/TestApiCase/test_payout_crypto_noraml.py
@@ -169,7 +169,7 @@ class TestPayoutCryptoNormalApi:
     def test_payout_crypto_normal_006(self):
         with allure.step("BTC确认Crypto提现交易"):
             data = {
-                "amount": "2.2",
+                "amount": "2.5",
                 "code": "BTC",
                 "address": "tb1q38mwu50xludgz4r52n2v0q6jwlysjgz4zkk3kl",
             }
@@ -188,7 +188,7 @@ class TestPayoutCryptoNormalApi:
     def test_payout_crypto_normal_007(self):
         with allure.step("ETH确认Crypto提现交易"):
             data = {
-                "amount": "30",
+                "amount": "40",
                 "code": "ETH",
                 "address": "0xA7185FBEE96B605709D9659894066dF21cc87f05",
                 "method": "ERC20"
@@ -208,7 +208,7 @@ class TestPayoutCryptoNormalApi:
     def test_payout_crypto_normal_008(self):
         with allure.step("USDT确认Crypto提现交易"):
             data = {
-                "amount": "51000",
+                "amount": "52000",
                 "code": "USDT",
                 "address": "0xA7185FBEE96B605709D9659894066dF21cc87f05",
                 "method": "ERC20"
