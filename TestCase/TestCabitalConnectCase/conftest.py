f311d64a239bfe6e038e63982b9f3eb236cf1455
Richard Wan <richard.wan@cabital.com>
2022-08-30 12:01:45 +0800

    123

--- TestCase/TestCabitalConnectCase/conftest.py
+++ TestCase/TestCabitalConnectCase/conftest.py
@@ -4,5 +4,6 @@ import sys
 
 @pytest.fixture(scope='session')
 def partner():
-    partner = sys.argv[2]
+    #partner = sys.argv[2]
+    partner = 'ace'
     return partner
--- TestCase/TestCabitalConnectCase/test_quote.py
+++ TestCase/TestCabitalConnectCase/test_quote.py
@@ -6,7 +6,7 @@ from Function.operate_sql import *
 class TestQuoteApi:
 
     @allure.title('test_quotes_001')
-    @allure.description('获取报价最新的报价')
+    @allure.description('寻价')
     def test_quotes_001(self, partner):
         with allure.step("签名"):
             unix_time = int(time.time())
@@ -27,16 +27,6 @@ class TestQuoteApi:
                 new_list.append(i['pair'])
             for i in new_list:
                 with allure.step("获取正向报价"):
-                    with allure.step("签名"):
-                        unix_time = int(time.time())
-                        nonce = generate_string(30)
-                        sign = ApiFunction.make_signature(unix_time=str(unix_time), method='GET', url='/api/v1/quotes/{}'.format(i),
-                                                          connect_type=partner, nonce=nonce)
-                        connect_headers['ACCESS-KEY'] = get_json(file='partner_info.json')[get_json()['env']][partner][
-                            'Partner_ID']
-                        connect_headers['ACCESS-SIGN'] = sign
-                        connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
-                        connect_headers['ACCESS-NONCE'] = nonce
                     r = session.request('GET', url='{}/quotes/{}'.format(connect_url, i), headers=connect_headers)
                     logger.info('币种对为{}'.format(i))
                     with allure.step("状态码和返回值"):
@@ -46,3 +36,67 @@ class TestQuoteApi:
                         assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
                     with allure.step("校验返回值"):
                         assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+                with allure.step("获取反向报价"):
+                    new_pair = '{}{}{}'.format(i.split('-')[1], '-', i.split('-')[0])
+                    r = session.request('GET', url='{}/quotes/{}'.format(connect_url, new_pair), headers=connect_headers)
+                    logger.info('币种对为{}'.format(new_pair))
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+
+    @allure.title('test_quotes_002')
+    @allure.description('换汇批量寻价')
+    def test_quotes_002(self, partner):
+        with allure.step("获取需要寻价的币种"):
+            for i in get_json(file='partner_info.json')[get_json()['env']][partner]['config_info']['currencies']:
+                pass
+
+
+
+    @allure.title('test_quotes_001')
+    @allure.description('寻价')
+    def test_quotes_003(self, partner):
+        with allure.step("签名"):
+            unix_time = int(time.time())
+            nonce = generate_string(30)
+            sign = ApiFunction.make_signature(unix_time=str(unix_time), method='GET', url='/api/v1/config', connect_type=partner, nonce=nonce)
+            connect_headers['ACCESS-KEY'] = get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID']
+            connect_headers['ACCESS-SIGN'] = sign
+            connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
+            connect_headers['ACCESS-NONCE'] = nonce
+        with allure.step("获取合作方的配置"):
+            r = session.request('GET', url='{}/config'.format(connect_url), headers=connect_headers)
+        with allure.step("状态码和返回值"):
+            logger.info('状态码是{}'.format(str(r.status_code)))
+            logger.info('返回值是{}'.format(str(r.text)))
+        with allure.step("获取最新的报价"):
+            new_list = []
+            for i in r.json()['pairs']:
+                new_list.append(i['pair'])
+            for i in new_list:
+                with allure.step("获取正向报价"):
+                    r = session.request('GET', url='{}/quotes/{}'.format(connect_url, i), headers=connect_headers)
+                    logger.info('币种对为{}'.format(i))
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+                with allure.step("获取反向报价"):
+                    new_pair = '{}{}{}'.format(i.split('-')[1], '-', i.split('-')[0])
+                    r = session.request('GET', url='{}/quotes/{}'.format(connect_url, new_pair), headers=connect_headers)
+                    logger.info('币种对为{}'.format(new_pair))
+                    with allure.step("状态码和返回值"):
+                        logger.info('状态码是{}'.format(str(r.status_code)))
+                        logger.info('返回值是{}'.format(str(r.text)))
+                    with allure.step("校验状态码"):
+                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
+                    with allure.step("校验返回值"):
+                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+
