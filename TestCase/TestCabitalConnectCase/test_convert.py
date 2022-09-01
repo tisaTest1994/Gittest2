6c6a4c5b032e8b9b434ae8865fa24ea92d4faac8
Richard Wan <richard.wan@cabital.com>
2022-09-01 15:39:16 +0800

    123

--- TestCase/TestCabitalConnectCase/test_convert.py
+++ TestCase/TestCabitalConnectCase/test_convert.py
@@ -10,22 +10,4 @@ class TestConvertApi:
         with allure.step("登录客户账户获得后续操作需要的token"):
             ApiFunction.add_headers()
 
-    @allure.title('test_convert_001')
-    @allure.description('不穿入partner_id')
-    def test_convert_001(self):
-        with allure.step("获取最新的报价"):
-            for i in ApiFunction.get_connect_cfx_list(self.url, headers, key='infinni games'):
-                with allure.step("获取正向报价"):
-                    r = session.request('GET', url='{}/quotes/{}'.format(self.url, i), headers=headers)
-                    with allure.step("校验状态码"):
-                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-                    with allure.step("校验返回值"):
-                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
-                with allure.step("获取反向报价"):
-                    new_pair = '{}{}{}'.format(i.split('-')[1], '-', i.split('-')[0])
-                    r = session.request('GET', url='{}/quotes/{}'.format(self.url, new_pair),
-                                        headers=headers)
-                    with allure.step("校验状态码"):
-                        assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
-                    with allure.step("校验返回值"):
-                        assert r.json()['quote'] is not None, "获取报价错误，返回值是{}".format(r.text)
+
