83641a5b5ebc17de59194d81c55015ccaf310e4a
Richard Wan <richard.wan@cabital.com>
2022-09-14 19:30:24 +0800

    1231

--- TestCase/TestCabitalConnectCase/conftest.py
+++ TestCase/TestCabitalConnectCase/conftest.py
@@ -4,6 +4,6 @@ import sys
 
 @pytest.fixture(scope='session')
 def partner():
-    partner = sys.argv[2]
-    # partner = 'latibac'
+    # partner = sys.argv[2]
+    partner = 'woo'
     return partner
