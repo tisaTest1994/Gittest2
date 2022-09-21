85970beffd57c5c6af1fc74cef8b94b738d946ef
Richard Wan <richard.wan@cabital.com>
2022-09-21 14:55:01 +0800

    no message

--- TestCase/TestApiCase/test_USDC.py
+++ TestCase/TestApiCase/test_USDC.py
@@ -21,7 +21,7 @@ class TestUSDCApi:
                     'accountNumber': '123119341897'
                 },
                 'amount': {
-                    'amount': '3.00',
+                    'amount': '23.00',
                     'currency': 'USD'
                 }
             }
@@ -64,12 +64,12 @@ class TestUSDCApi:
             'Authorization'] = "Bearer QVBJX0tFWTozMzY4OGI0ZTdjYTgzYjlmODU2ODIzNjlhZTU2OGEzZTplMWNjMWQyMGQxNThiMTUwMDU5NzI0N2ZjMmYxZTA4OQ=="
         with allure.step("payin"):
             data = {
-                'trackingRef': 'CIR25EANVT',
+                'trackingRef': 'CIR2JN4JRH',
                 'beneficiaryBank': {
-                    'accountNumber': '123119341897'
+                    'accountNumber': '123373874035'
                 },
                 'amount': {
-                    'amount': '3.00',
+                    'amount': '300.00',
                     'currency': 'USD'
                 }
             }
@@ -89,10 +89,10 @@ class TestUSDCApi:
             password=get_json()['operate_admin_account']['password'], type='operate')
         with allure.step("payout"):
             data = {
-                "account_id": "700dca34-1e6f-408b-903d-e37d0fcfd615",
+                "account_id": "a27f5914-9416-48be-bf56-bc9eeac18a64",
                 "legal_entity": 1,
                 "code": "USD",
-                "amount": "20",
+                "amount": "130",
                 "payment_method": "Wire",
                 "bank_account_id": "dfaeabff-3d48-4e5b-9d25-950c18782998",
                 "email": "external.qa@cabital.com"
@@ -103,7 +103,7 @@ class TestUSDCApi:
                 logger.info('状态码是{}'.format(str(r.status_code)))
                 logger.info('返回值是{}'.format(str(r.text)))
             with allure.step("校验状态码"):
-                assert r.status_code == 201, "http 状态码不对，目前状态码是{}".format(r.status_code)
+                assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
 
     @allure.title('test_usdc_001')
     @allure.description('USDC 个人 ERC20 payout')
@@ -116,13 +116,37 @@ class TestUSDCApi:
                 "account_id": "700dca34-1e6f-408b-903d-e37d0fcfd615",
                 "legal_entity": 1,
                 "code": "USDC",
-                "amount": "40.231231",
+                "amount": "40.22",
                 "chain": "ETH",
                 "address": "0x465d39f446f3EE9867B318A5bB98EF7dA796DFbA"
             }
             r = session.request('POST', url='https://opapi.cabital.io/api/v1/operatorapi/txns/payout/create/crypto',
                                 data=json.dumps(data), headers=headers)
             with allure.step("状态码和返回值"):
+                logger.info('状态码是{}'.format(str(r.status_code)))
+                logger.info('返回值是{}'.format(str(r.text)))
+            with allure.step("校验状态码"):
+                assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+
+    @allure.title('test_usdc_002')
+    @allure.description('USDC 商户 ERC20 payout')
+    def test_usdc_002(self):
+        headers['Authorization'] = "Bearer " + ApiFunction.get_account_token(
+            account=get_json()['operate_admin_account']['email'],
+            password=get_json()['operate_admin_account']['password'], type='operate')
+        with allure.step("payout"):
+            data = {
+                "account_id": "a27f5914-9416-48be-bf56-bc9eeac18a64",
+                "legal_entity": 1,
+                "code": "USDC",
+                "amount": "41.22",
+                "chain": "ETH",
+                "address": "0x60CB212F15dcEf185240d43C4e162C730fA8a9C2"
+            }
+            r = session.request('POST', url='https://opapi.cabital.io/api/v1/operatorapi/txns/payout/create/crypto',
+                                data=json.dumps(data), headers=headers)
+            with allure.step("状态码和返回值"):
+                logger.info('trace id是{}'.format(str(r.headers['Traceparent'])))
                 logger.info('状态码是{}'.format(str(r.status_code)))
                 logger.info('返回值是{}'.format(str(r.text)))
             with allure.step("校验状态码"):
