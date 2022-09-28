16bb7b238ef0523db713e2e9178e71fb07816fb3
Richard Wan <richard.wan@cabital.com>
2022-09-28 14:30:06 +0800

    123

--- TestCase/TestCabitalConnectCase/test_config.py
+++ TestCase/TestCabitalConnectCase/test_config.py
@@ -21,7 +21,7 @@ class TestConfigApi:
         with allure.step("状态码和返回值"):
             logger.info('状态码是{}'.format(str(r.status_code)))
             logger.info('返回值是{}'.format(str(r.text)))
-            sleep(1)
+            sleep(2)
         with allure.step("校验状态码"):
             assert r.status_code == 200, "http状态码不对，目前状态码是{}".format(r.status_code)
         with allure.step("校验返回值"):
