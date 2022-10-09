cf54c2eb56667fba6e83b65b7bc7448ad138e2b6
Richard Wan <richard.wan@cabital.com>
2022-10-09 14:49:30 +0800

    update

--- TestCase/TestCabitalConnectCase/test_widget.py
+++ TestCase/TestCabitalConnectCase/test_widget.py
@@ -28,7 +28,9 @@ class TestWidgetApi:
                                            "CHF": {"min": "10", "max": "200000"}, "ETH": {"min": "0.002", "max": "100"},
                                            "EUR": {"min": "10", "max": "200000"}, "GBP": {"min": "10", "max": "200000"},
                                            "USDT": {"min": "10", "max": "200000"},
-                                           "VND": {"min": "250000", "max": "5000000000"}}}, '不传入partner_id, 取得全部conversion limit错误，返回值是{}'.format(r.text)
+                                           "VND": {"min": "250000",
+                                                   "max": "5000000000"}}}, '不传入partner_id, 取得全部conversion limit错误，返回值是{}'.format(
+                r.text)
 
     @allure.title('test_widget_002')
     @allure.description('传入partner_id, 取得全部conversion limit')
@@ -50,7 +52,9 @@ class TestWidgetApi:
                     if i['symbol'] == y:
                         config_conversion = i['config']['conversion']
                         del config_conversion['allow']
-                        assert r.json()['limits'][y] == config_conversion, '传入partner_id, 取得全部conversion limit错误，币种是{}, config 配置内的值是{}, 接口返回值是{}'.format(y, config_conversion, r.json()['limits'][y])
+                        assert r.json()['limits'][
+                                   y] == config_conversion, '传入partner_id, 取得全部conversion limit错误，币种是{}, config 配置内的值是{}, 接口返回值是{}'.format(
+                            y, config_conversion, r.json()['limits'][y])
 
     @allure.title('test_widget_003')
     @allure.description('transfer debit 交易')
@@ -67,14 +71,20 @@ class TestWidgetApi:
                         "amount": giveAmount(i['symbol']),
                         "code": i['symbol'],
                         "direction": "DEBIT",
-                        "account_vid": get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['account_vid'],
-                        "user_ext_ref": get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['user_ref_id']
+                        "account_vid":
+                            get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                'richard']['account_vid'],
+                        "user_ext_ref":
+                            get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                'richard']['user_ref_id']
                     }
                     with allure.step("获取2fa code"):
                         mfaVerificationCode = get_mfa_code('richard')
                         headers['X-Mfa-Otp'] = str(mfaVerificationCode)
                     with allure.step("transfer 交易"):
-                        r = session.request('POST', url='{}/connect/{}/transfer'.format(env_url, get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID']), data=json.dumps(data), headers=headers)
+                        r = session.request('POST', url='{}/connect/{}/transfer'.format(env_url, get_json(
+                            file='partner_info.json')[get_json()['env']][partner]['Partner_ID']), data=json.dumps(data),
+                                            headers=headers)
                     with allure.step("状态码和返回值"):
                         logger.info('状态码是{}'.format(str(r.status_code)))
                         logger.info('返回值是{}'.format(str(r.text)))
@@ -88,11 +98,12 @@ class TestWidgetApi:
                     with allure.step("获得transfer后金额"):
                         wallet_balance_latest = ApiFunction.get_crypto_number(type=i['symbol'])
                     assert Decimal(wallet_balance_old) - Decimal(data['amount']) == Decimal(
-                        wallet_balance_latest), 'transfer币种是{},transfer前金额是{},transfer金额是{}，transfer后金额是{}'.format(i['symbol'],
-                                                                                                                   wallet_balance_old,
-                                                                                                                   data[
-                                                                                                                       'amount'],
-                                                                                                                   wallet_balance_latest)
+                        wallet_balance_latest), 'transfer币种是{},transfer前金额是{},transfer金额是{}，transfer后金额是{}'.format(
+                        i['symbol'],
+                        wallet_balance_old,
+                        data[
+                            'amount'],
+                        wallet_balance_latest)
                     with allure.step("确认划转"):
                         if r.json()['status'] == 1:
                             external_id = generate_string(25)
@@ -106,12 +117,19 @@ class TestWidgetApi:
                             with allure.step("验签"):
                                 unix_time = int(time.time())
                                 nonce = generate_string(20) + str(time.time()).split('.')[0]
-                                sign = ApiFunction.make_signature(unix_time=str(unix_time), method='PUT', url='/api/v1/accounts/{}/transfers/{}'.format(account_vid, transfer_id), connect_type=partner, nonce=nonce, body=json.dumps(data))
-                                connect_headers['ACCESS-KEY'] = get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID']
+                                sign = ApiFunction.make_signature(unix_time=str(unix_time), method='PUT',
+                                                                  url='/api/v1/accounts/{}/transfers/{}'.format(
+                                                                      account_vid, transfer_id), connect_type=partner,
+                                                                  nonce=nonce, body=json.dumps(data))
+                                connect_headers['ACCESS-KEY'] = \
+                                get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID']
                                 connect_headers['ACCESS-SIGN'] = sign
                                 connect_headers['ACCESS-TIMESTAMP'] = str(unix_time)
                                 connect_headers['ACCESS-NONCE'] = nonce
-                            r = session.request('PUT', url='{}/accounts/{}/transfers/{}'.format(connect_url, account_vid, transfer_id), data=json.dumps(data), headers=connect_headers)
+                            r = session.request('PUT',
+                                                url='{}/accounts/{}/transfers/{}'.format(connect_url, account_vid,
+                                                                                         transfer_id),
+                                                data=json.dumps(data), headers=connect_headers)
                             with allure.step("校验状态码"):
                                 assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
                             with allure.step("校验返回值"):
@@ -123,15 +141,19 @@ class TestWidgetApi:
         with allure.step("获得用户信息"):
             params = {
                 'partner_key': get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID'],
-                'user_ext_ref': get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['user_ref_id'],
-                'email': get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['email'],
+                'user_ext_ref':
+                    get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard'][
+                        'user_ref_id'],
+                'email': get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard'][
+                    'email'],
                 'redirect_url': 'https://www.baidu.com',
                 'feature': 'onboarding'
             }
             r1 = session.request('GET', url='{}/partner/link'.format(connect_url), params=params, headers=headers)
             with allure.step("验签"):
                 sign = ApiFunction.get_link_sign(url=r1.url, partner=partner)
-                connect_headers['ACCESS-SECRET'] = get_json(file='partner_info.json')[get_json()['env']][partner]['Secret_Key']
+                connect_headers['ACCESS-SECRET'] = get_json(file='partner_info.json')[get_json()['env']][partner][
+                    'Secret_Key']
                 params['signature'] = sign
             r = session.request('GET', url='{}/partner/link'.format(connect_url), params=params, headers=headers)
             with allure.step("状态码和返回值"):
@@ -235,10 +257,18 @@ class TestWidgetApi:
                                         "destination": {
                                             "destination_type": 2,
                                             "transfer_txn": {
-                                                "partner_name": get_json(file='partner_info.json')[get_json()['env']][partner]['partner_name'],
-                                                "partner_id": get_json(file='partner_info.json')[get_json()['env']][partner]['Partner_ID'],
-                                                "user_ext_ref": get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['user_ref_id'],
-                                                "account_vid": get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard']['account_vid'],
+                                                "partner_name":
+                                                    get_json(file='partner_info.json')[get_json()['env']][partner][
+                                                        'partner_name'],
+                                                "partner_id":
+                                                    get_json(file='partner_info.json')[get_json()['env']][partner][
+                                                        'Partner_ID'],
+                                                "user_ext_ref":
+                                                    get_json(file='partner_info.json')[get_json()['env']][partner][
+                                                        'account_vid_list']['richard']['user_ref_id'],
+                                                "account_vid":
+                                                    get_json(file='partner_info.json')[get_json()['env']][partner][
+                                                        'account_vid_list']['richard']['account_vid'],
                                                 "receive": {
                                                     "code": i['symbol'],
                                                     "amount": crypto_list['buy_amount']
@@ -688,3 +718,102 @@ class TestWidgetApi:
                     with allure.step("校验返回值"):
                         assert r.json()['status'] == 1, "币种兑{},地区{},ccy是spend,checkout支付错误，当前返回值是{}".format(
                             z, x, r.text)
+
+    @allure.title('test_widget_007')
+    @allure.description('transfer debit 交易小于最小值')
+    def test_widget_007(self, partner):
+        with allure.step("划转"):
+            for i in get_json(file='partner_info.json')[get_json()['env']][partner]['config_info']['currencies']:
+                if i['config']['transfer_debit']['allow'] is True:
+                    if i['config']['transfer_debit']['min'] != '-1':
+                        data = {
+                            "amount": str(Decimal(i['config']['transfer_debit']['min']) - Decimal(0.01)),
+                            "code": i['symbol'],
+                            "direction": "DEBIT",
+                            "account_vid":
+                                get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                    'richard']['account_vid'],
+                            "user_ext_ref":
+                                get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                    'richard']['user_ref_id']
+                        }
+                        with allure.step("获取2fa code"):
+                            mfaVerificationCode = get_mfa_code('richard')
+                            headers['X-Mfa-Otp'] = str(mfaVerificationCode)
+                        with allure.step("transfer 交易"):
+                            r = session.request('POST', url='{}/connect/{}/transfer'.format(env_url, get_json(
+                                file='partner_info.json')[get_json()['env']][partner]['Partner_ID']),
+                                                data=json.dumps(data), headers=headers)
+                        with allure.step("状态码和返回值"):
+                            logger.info('状态码是{}'.format(str(r.status_code)))
+                            logger.info('返回值是{}'.format(str(r.text)))
+                        with allure.step("校验状态码"):
+                            assert r.status_code == 400, "http 状态码不对，目前状态码是{}".format(r.status_code)
+                        with allure.step("校验返回值"):
+                            assert r.json()['code'] == 'PA003', "transfer 使用小于最小值的订单交易错误，返回值是{}".format(r.text)
+
+    @allure.title('test_widget_008')
+    @allure.description('transfer debit 交易大于最大值')
+    def test_widget_008(self, partner):
+        with allure.step("划转"):
+            for i in get_json(file='partner_info.json')[get_json()['env']][partner]['config_info']['currencies']:
+                if i['config']['transfer_debit']['allow'] is True:
+                    if i['config']['transfer_debit']['max'] != '-1':
+                        data = {
+                            "amount": str(Decimal(i['config']['transfer_debit']['max']) + Decimal(0.01)),
+                            "code": i['symbol'],
+                            "direction": "DEBIT",
+                            "account_vid":
+                                get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                    'richard']['account_vid'],
+                            "user_ext_ref":
+                                get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list'][
+                                    'richard']['user_ref_id']
+                        }
+                        with allure.step("获取2fa code"):
+                            mfaVerificationCode = get_mfa_code('richard')
+                            headers['X-Mfa-Otp'] = str(mfaVerificationCode)
+                        with allure.step("transfer 交易"):
+                            r = session.request('POST', url='{}/connect/{}/transfer'.format(env_url, get_json(
+                                file='partner_info.json')[get_json()['env']][partner]['Partner_ID']),
+                                                data=json.dumps(data), headers=headers)
+                        with allure.step("状态码和返回值"):
+                            logger.info('状态码是{}'.format(str(r.status_code)))
+                            logger.info('返回值是{}'.format(str(r.text)))
+                        with allure.step("校验状态码"):
+                            assert r.status_code == 400, "http 状态码不对，目前状态码是{}".format(r.status_code)
+                        with allure.step("校验返回值"):
+                            assert r.json()['code'] == 'PA004', "transfer 使用大于最大值的订单交易错误，返回值是{}".format(r.text)
+
+    @allure.title('test_widget_009')
+    @allure.description('transfer debit 使用不支持的币种')
+    def test_widget_009(self, partner):
+        with allure.step("划转"):
+            data = {
+                "amount": '50',
+                "code": 'CHF',
+                "direction": "DEBIT",
+                "account_vid":
+                    get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard'][
+                        'account_vid'],
+                "user_ext_ref":
+                    get_json(file='partner_info.json')[get_json()['env']][partner]['account_vid_list']['richard'][
+                        'user_ref_id']
+            }
+            with allure.step("获取2fa code"):
+                mfaVerificationCode = get_mfa_code('richard')
+                headers['X-Mfa-Otp'] = str(mfaVerificationCode)
+            with allure.step("transfer 交易"):
+                r = session.request('POST', url='{}/connect/{}/transfer'.format(env_url,
+                                                                                get_json(file='partner_info.json')[
+                                                                                    get_json()['env']][partner][
+                                                                                    'Partner_ID']),
+                                    data=json.dumps(data), headers=headers)
+            with allure.step("状态码和返回值"):
+                logger.info('状态码是{}'.format(str(r.status_code)))
+                logger.info('返回值是{}'.format(str(r.text)))
+            with allure.step("校验状态码"):
+                assert r.status_code == 400, "http 状态码不对，目前状态码是{}".format(r.status_code)
+            with allure.step("校验返回值"):
+                assert r.json()['code'] == 'PA019', "transfer 交易错误，返回值是{}".format(r.text)
+
