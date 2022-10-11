0a460d90c8837741e33039a53f0bfb74ee4f6207
Richard Wan <richard.wan@cabital.com>
2022-10-11 14:50:26 +0800

    fix bug

--- TestCase/TestApiCase/test_payin_crypto.py
+++ TestCase/TestApiCase/test_payin_crypto.py
@@ -23,9 +23,9 @@ class TestPayInCryptoApi:
                 with allure.step("校验返回值"):
                     assert r.json()[0]['code'] == i, "查询数字货币转入地址错误，返回值是{}".format(r.text)
 
-    @allure.title('test_pay_in_002')
+    @allure.title('test_pay_in_crypto_002')
     @allure.description('使用错误币种查询数字货币转入地址')
-    def test_pay_in_002(self):
+    def test_pay_in_crypto_002(self):
         with allure.step("国际化"):
             for i in get_json()['language_list']:
                 headers['locale'] = i
@@ -36,43 +36,63 @@ class TestPayInCryptoApi:
                     r = session.request('GET', url='{}/pay/deposit/addresses'.format(env_url), params=params,
                                         headers=headers)
                     with allure.step("校验状态码"):
-                        assert r.status_code == 400, "http 状态码不对，目前状态码是{}".format(r.status_code)
+                        assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
                     with allure.step("校验返回值"):
-                        assert r.json()['code'] == '103003', "使用错误币种查询数字货币转入地址错误，返回值是{}".format(r.text)
+                        assert r.json() == [], "使用错误币种查询数字货币转入地址错误，返回值是{}".format(r.text)
 
-    @allure.title('test_pay_in_003')
-    @allure.description('使用指定链查询数字货币转入地址')
-    def test_pay_in_003(self):
+    @allure.title('test_pay_in_crypto_003')
+    @allure.description('使用指定链ETH查询数字货币转入地址')
+    def test_pay_in_crypto_003(self):
         with allure.step("查询转入记录"):
             data = {
                 'code': 'ETH',
-                'method': 'ERC20'
+                'chain': 'ETH'
             }
             r = session.request('GET', url='{}/pay/deposit/addresses'.format(env_url), params=data, headers=headers)
+            with allure.step("状态码和返回值"):
+                logger.info('trace id是{}'.format(str(r.headers['Traceparent'])))
+                logger.info('状态码是{}'.format(str(r.status_code)))
+                logger.info('返回值是{}'.format(str(r.text)))
             with allure.step("校验状态码"):
                 assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
             with allure.step("校验返回值"):
-                assert r.json()[0]['code'] == data['code'], "使用指定链查询数字货币转入地址错误，返回值是{}".format(r.text)
-                assert r.json()[0]['method'] == data['method'], "使用指定链查询数字货币转入地址错误，返回值是{}".format(r.text)
+                assert r.json()[0]['code'] == data['code'], "使用指定链ETH查询数字货币转入地址错误，返回值是{}".format(r.text)
 
-    @allure.title('test_pay_in_004')
+    # @allure.title('test_pay_in_crypto_004')
+    # @allure.description('使用指定链TRX查询数字货币转入地址')
+    # def test_pay_in_crypto_004(self):
+    #     with allure.step("查询转入记录"):
+    #         data = {
+    #             'code': 'ETH',
+    #             'chain': 'TRX'
+    #         }
+    #         r = session.request('GET', url='{}/pay/deposit/addresses'.format(env_url), params=data, headers=headers)
+    #         with allure.step("状态码和返回值"):
+    #             logger.info('trace id是{}'.format(str(r.headers['Traceparent'])))
+    #             logger.info('状态码是{}'.format(str(r.status_code)))
+    #             logger.info('返回值是{}'.format(str(r.text)))
+    #         with allure.step("校验状态码"):
+    #             assert r.status_code == 200, "http 状态码不对，目前状态码是{}".format(r.status_code)
+    #         with allure.step("校验返回值"):
+    #             assert r.json()[0]['code'] == data['code'], "使用指定链查询数字货币转入地址错误，返回值是{}".format(r.text)
+
+    @allure.title('test_pay_in_crypto_005')
     @allure.description('使用错误链查询数字货币转入地址')
-    def test_pay_in_004(self):
+    def test_pay_in_crypto_005(self):
         with allure.step("查询转入记录"):
             data = {
                 'code': 'ETH',
-                'method': 'ER124141'
+                'chain': 'ER124141'
             }
             r = session.request('GET', url='{}/pay/deposit/addresses'.format(env_url), params=data, headers=headers)
             with allure.step("校验状态码"):
                 assert r.status_code == 400, "http 状态码不对，目前状态码是{}".format(r.status_code)
             with allure.step("校验返回值"):
-                print(r.json())
-                assert r.json()['code'] == '103003', "查询不到转入地址记录（使用错误链查询）错误，返回值是{}".format(r.text)
+                assert r.json()['code'] == '100000', "查询不到转入地址记录（使用错误链查询）错误，返回值是{}".format(r.text)
 
-    @allure.title('test_pay_in_005')
+    @allure.title('test_pay_in_crypto_006')
     @allure.description('获得数字货币充值币种')
-    def test_pay_in_005(self):
+    def test_pay_in_crypto_006(self):
         with allure.step("充值币种"):
             r = session.request('GET', url='{}/pay/deposit/ccy/{}'.format(env_url, 'crypto'), headers=headers)
             with allure.step("校验状态码"):
